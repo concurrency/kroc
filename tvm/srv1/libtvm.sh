5e5afdc2ded67890e2fc0913fc0befc3e76bf056
C.G. Ritson <cgr@kent.ac.uk>
2008-01-30 11:55:50 +0000

    Shell script to cross compile a copy of libtvm for the Blackfin, into a directory call 'libtvm' in the srv1 tree.

---
+++ tvm/srv1/libtvm.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+[ -n "$CC" ] && X_CC=$CC || X_CC=bfin-elf-gcc
+X_CFLAGS="-D__ADSPBF537__ -D__ADSPLPBLACKFIN__ $CFLAGS"
+
+CONF_OPTS="--host=bfin-elf --disable-occam-pi"
+BUILD="libtvm"
+EPATH=$PWD
+
+if [ -n "$1" ]; then
+	SRC=$1
+else
+	SRC=$EPATH/../../runtime/libtvm
+fi
+
+[ ! -e $BUILD ] && mkdir $BUILD
+
+if [ -d $SRC ]; then
+	cd $SRC
+	[ ! -x configure ] && autoreconf -f -i
+	if [ -x configure ]; then
+		[ -e Makefile ] && make distclean
+		cd $EPATH
+		if [ -d $BUILD ]; then
+			cd $BUILD && CFLAGS="$X_CFLAGS" CC="$X_CC" $SRC/configure $CONF_OPTS && make
+		else
+			echo "Error: build directory $BUILD doesn't exist."
+		fi
+	else
+		echo "Error: configure script doesn't exist in source directory ($SRC)."
+	fi
+	cd $EPATH
+else
+	echo "Error: source directory $SRC doesn't exist."
+fi
+
