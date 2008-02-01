1d926d5754a91beee0ce16958cfee202d7b99ff1
C.G. Ritson <cgr@kent.ac.uk>
2008-02-01 15:06:48 +0000

    Implement UART0 input as a placed (external) channel. Make the demo firmwire respond to keys '1' and '2', twiddling the LEDs.

--- tvm/srv1/main.c
+++ tvm/srv1/main.c
@@ -14,10 +14,6 @@
 
 static char version_string[] = "TVM SRV-1 Blackfin w/C interpreter - " __TIME__ " - " __DATE__;
 
-void handle_int10 (void)
-{
-}
-
 /*{{{  Surveyor support code */
 static void init_io (void) {
 	*pPORTGIO_DIR	= 0x0300;	// LEDs (PG8 and PG9)
@@ -77,6 +73,139 @@ static tvm_t 		tvm;
 static tvm_ectx_t 	firmware_ctx, user_ctx;
 /*}}}*/
 
+/*{{{  UART functionality */
+static volatile WORDPTR 	uart0_channel 	= (WORDPTR) NOT_PROCESS_P;
+static volatile BYTEPTR		uart0_ptr	= (BYTEPTR) NULL_P;
+static volatile unsigned char 	uart0_buffer	= '\0';
+static volatile int		uart0_pending	= 0;
+
+static void init_uart (void)
+{
+	unsigned char temp;
+
+	/* Enable UART pins on port F */
+	*pPORTF_FER |= 0x000f;
+
+	/* Enable UART0 clocks */
+	*pUART0_GCTL = UCEN;
+	/* Switch on divisor programming */
+	*pUART0_LCR = DLAB;
+	/* Program divisor */
+	*pUART0_DLL = UART0_DIVIDER;
+	*pUART0_DLH = UART0_DIVIDER >> 8;
+	/* Set operational mode (disables divisor programming) */
+	*pUART0_LCR = WLS(8); /* 8 bit, no parity, one stop bit */
+
+	/* Reads to clear possible pending errors / irqs */
+	temp = *pUART0_RBR;
+	temp = *pUART0_LSR;
+	temp = *pUART0_IIR;
+	SSYNC;
+
+	/* Enable receive interrupts */
+	*pSIC_IMASK |= IRQ_DMA8;
+	*pUART0_IER |= ERBFI;
+	SSYNC;
+
+	/* The same for UART1 */
+	#if 0
+	*pUART1_GCTL = UCEN;
+	*pUART1_LCR = DLAB;
+	*pUART1_DLL = UART1_DIVIDER;
+	*pUART1_DLH = UART1_DIVIDER >> 8;
+	*pUART1_LCR = WLS(8); /* 8 bit, no parity, one stop bit */
+
+	temp = *pUART1_RBR;
+	temp = *pUART1_LSR;
+	temp = *pUART1_IIR;
+	SSYNC;
+	#endif
+}
+
+void handle_int10 (void)
+{
+	unsigned char buffer;
+
+	/* Raise (clear) CTS */
+	*pPORTHIO	= *pPORTHIO | 0x0400;
+
+	/* Read character (clears interrupt condition) */
+	buffer		= *pUART0_RBR;
+
+	/* Is anything waiting for the character? */
+	if (uart0_ptr != (BYTEPTR) NULL_P) {
+		/* Yes; give it the data and trigger requeue */
+		write_byte (uart0_ptr, (BYTE) buffer);
+		uart0_ptr		= (BYTEPTR) NULL_P;
+		firmware_ctx.sflags	|= SFLAG_INTR;
+		user_ctx.sflags		|= SFLAG_INTR;
+	} else {
+		/* No; buffer the character */
+		uart0_buffer = buffer;
+		uart0_pending++;
+	}
+}
+
+static int uart0_in (ECTX ectx, WORD count, BYTEPTR pointer)
+{
+	unsigned short imask;
+	int reschedule;
+	
+	/* Disable interrupts */
+	__asm__ __volatile__ ("cli %0" : "=r" (imask));
+
+	if (uart0_pending) {
+		write_byte (pointer, (BYTE) uart0_buffer);
+		uart0_pending	= 0;
+		reschedule	= 0;
+	} else {
+		uart0_channel	= ectx->wptr;
+		uart0_ptr	= pointer;
+		reschedule	= 1;
+	}
+
+	/* Enable (restore) interrupts */
+	__asm__ __volatile__ ("sti %0" : : "r" (imask));
+
+	if (reschedule) {
+		/* Lower (set) CTS */
+		*pPORTHIO = *pPORTHIO & (~0x0400);
+		/* Save instruction pointer */
+		WORKSPACE_SET (ectx->wptr, WS_IPTR, (WORD) ectx->iptr);
+		/* Reschedule */
+		return ectx->run_next_on_queue (ectx);
+	} else {
+		return ECTX_CONTINUE;
+	}
+}
+/*}}}*/
+
+/*{{{  scheduling code */
+static void srv_modify_sync_flags(ECTX ectx, WORD set, WORD clear)
+{
+	unsigned short imask;
+	
+	/* Disable interrupts */
+	__asm__ __volatile__ ("cli %0" : "=r" (imask));
+
+	ectx->sflags = (ectx->sflags & (~clear)) | set;
+
+	/* Enable (restore) interrupts */
+	__asm__ __volatile__ ("sti %0" : : "r" (imask));
+}
+/*}}}*/ 
+
+/*{{{  External channel definitions */
+static EXT_CHAN_ENTRY	ext_chans[] = {
+	{ 
+		.typehash 	= 0,
+		.in 		= uart0_in, 
+		.out 		= NULL
+	}
+};
+static const int	ext_chans_length = sizeof(ext_chans) / sizeof(EXT_CHAN_ENTRY);
+/*}}}*/
+
 #include "firmware.h"
 
 static WORD memory[256];
@@ -90,7 +219,7 @@ int main (void) {
 	int ret;
 
 	clear_sdram ();
-	init_uarts ();
+	init_uart ();
 	init_io ();
 	init_time ();
 
@@ -101,11 +230,15 @@ int main (void) {
 	
 	/* Initialise firmware execution context */
 	tvm_ectx_init (&tvm, firmware);
-	firmware->get_time 	= srv_get_time;
+	firmware->get_time 		= srv_get_time;
+	firmware->modify_sync_flags	= srv_modify_sync_flags;
+	firmware->ext_chan_table	= ext_chans;
+	firmware->ext_chan_table_length	= ext_chans_length;
 
 	/* Initialise user execution context */
 	tvm_ectx_init (&tvm, user);
-	user->get_time 		= srv_get_time;
+	user->get_time 			= srv_get_time;
+	user->modify_sync_flags		= srv_modify_sync_flags;
 
 	/* Setup memory and initial workspace */
 	tvm_ectx_layout (firmware, base, "", 0, ws_size, vs_size, ms_size, &size, &ws, &vs, &ms);
@@ -115,9 +248,22 @@ int main (void) {
 	uart0SendChar ('\n');
 	delay_ms (1000);
 	
-	do {
+	for (;;) {
 		ret = tvm_run (firmware);
-	} while (ret == ECTX_SLEEP);
+
+		if (ret == ECTX_SLEEP) {
+			continue;
+		} else if (ret == ECTX_EMPTY && uart0_channel != (WORDPTR) NOT_PROCESS_P) {
+			continue;
+		} else if (ret == ECTX_INTERRUPT) {
+			firmware->sflags	&= ~(SFLAG_INTR);
+			user->sflags		&= ~(SFLAG_INTR);
+			firmware->add_to_queue (firmware, uart0_channel);
+			uart0_channel		= (WORDPTR) NOT_PROCESS_P;
+		} else {
+			break;
+		}
+	}
 	
 	uart0SendString ((unsigned char *) "##Out of runloop, state: ");
 	uart0SendChar ((unsigned char) ret);
