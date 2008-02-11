15ca380838115f9335ab2b9b7a3871e15509f1ad
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-02-11 13:40:28 +0000

    Shift up deadlock detection support code from the SRV-1 code base, for use accross wrappers. Convert "interpreter.c" to "my style", so there's one less file for me to keep switching styles in.

--- runtime/libtvm/tvm.h
+++ runtime/libtvm/tvm.h
@@ -58,20 +58,21 @@ struct _tvm_t {
 /*}}}*/
 
 /*{{{  Interpreter API */
-extern int tvm_init(tvm_t *tvm);
-extern void tvm_ectx_reset(ECTX ectx);
-extern void tvm_ectx_init(tvm_t *tvm, ECTX ectx);
-extern void tvm_ectx_layout(ECTX ectx, WORDPTR base,
+extern int tvm_init (tvm_t *tvm);
+extern void tvm_ectx_reset (ECTX ectx);
+extern void tvm_ectx_init (tvm_t *tvm, ECTX ectx);
+extern void tvm_ectx_layout (ECTX ectx, WORDPTR base,
 				const char *tlp_fmt, const int tlp_argc,
 				WORD ws_size, WORD vs_size, WORD ms_size,
 				WORD *size, WORDPTR *ws, WORDPTR *vs, WORDPTR *ms);
-extern int tvm_ectx_install_tlp(ECTX ectx, BYTEPTR code,
+extern int tvm_ectx_install_tlp (ECTX ectx, BYTEPTR code,
 				WORDPTR ws, WORDPTR vs, WORDPTR ms,
 				const char *fmt, int argc, const WORD argv[]);
-extern void tvm_ectx_disconnect(ECTX ectx);
-extern int tvm_dispatch(ECTX ectx);
-extern int tvm_run(ECTX ectx);
-extern int tvm_run_count(ECTX ectx, UWORD count);
+extern void tvm_ectx_disconnect (ECTX ectx);
+extern int tvm_ectx_waiting_on (ECTX ectx, WORDPTR ws_base, WORD ws_len);
+extern int tvm_dispatch (ECTX ectx);
+extern int tvm_run (ECTX ectx);
+extern int tvm_run_count (ECTX ectx, UWORD count);
 /*}}}*/
 
 #endif /* TVM_H */
