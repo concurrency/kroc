5696b43f1a38bc65f7bd1ceb217224938f136258
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-02-13 12:19:39 +0000

    This should be all that's required to provide dynamic memory to the user. Doing the new camera stuff is another round of work and integration on top of this.

--- tvm/srv1/include/memory_map.h
+++ tvm/srv1/include/memory_map.h
@@ -2,6 +2,10 @@
 #define FIRMWARE_SECTOR	0x00020000  /* address in SPI flash of firmware */
 #define FLASH_SECTOR	0x00040000  /* address in SPI flash of user flash sector */
 
+/* 0MiB - 32MiB SDRAM */
+#define SDRAM_BOTTOM	0x00000000  /* address of the base of SDRAM */
+#define SDRAM_TOP	0x02000000  /* address of the top of SDRAM */
+
 /* 1MiB - 7MiB DMA buffers */
 #define DMA_BUF1	0x00100000  /* address in SDRAM for DMA transfer of frames from camera */
 #define DMA_BUF2	0x00400000  /*   second DMA buffer for double buffering */
