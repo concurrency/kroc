c67b7ba7ac6fd44eed3051f5ad8c13561b59d3dc
ats <ats@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-03-04 17:41:35 +0000

    Make ukcthreads_types.h more C99-friendly: if you include it after stdbool.h, it won't try to redefine bool, true and false.

--- runtime/ccsp/include/ukcthreads_types.h
+++ runtime/ccsp/include/ukcthreads_types.h
@@ -2,6 +2,7 @@
  *	Type definitions
  *	Copyright (C) 1998 Jim Moores
  *	Modifications (C) 2007 Carl Ritson <cgr@kent.ac.uk>
+ *	              (C) 2008 Adam Sampson <ats@offog.org>
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -21,19 +22,20 @@
 #ifndef UKCTHREADS_TYPES_H
 #define UKCTHREADS_TYPES_H
 
+typedef unsigned int 	word;
+typedef unsigned char 	byte;
+
+#if !((defined __cplusplus) || (defined __bool_true_false_are_defined))
 #define true 	1
-#define TRUE 	1
 #define false 	0
-#define FALSE 	0
+typedef word	 	bool;
+#endif
+
+#define TRUE 	true
+#define FALSE 	false
 
 #define one_if_true(x) ((x) & true)
 #define zero_if_false(x) ((x))
-
-typedef unsigned int 	word;
-typedef unsigned char 	byte;
-#ifndef __cplusplus
-typedef word	 	bool;
-#endif
 
 #endif /* UKCTHREADS_TYPES_H */
 
