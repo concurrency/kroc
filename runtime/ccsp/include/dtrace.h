9770625f64dee077610c72e50a73c172dcc983d1
fred <fred@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-07-13 12:24:16 +0000

    Merged revisions 4933-4939 via svnmerge from https://projects.cs.kent.ac.uk/projects/kroc/svn/kroc/trunk
    ........
      r4933 | ats | 2008-05-22 11:18:48 +0100 (Thu, 22 May 2008) | 5 lines
      
      In X_mt_resize, when copying the existing contents of a data array, use the
      minimum of the old and new sizes when computing the amount to memcpy.
      (Previously it always used the new size, so it would blow up when extending a
      big array as it tried to read past the end of the old data.)
    ........
      r4934 | ats | 2008-05-22 16:10:40 +0100 (Thu, 22 May 2008) | 2 lines
      
      Add a "rastertext" module for drawing bitmap text onto a raster.
    ........
      r4935 | fred | 2008-06-25 02:43:47 +0100 (Wed, 25 Jun 2008) | 2 lines
      
      Added ellipse filling code to rastergraphics (from David Wood, modified for clipping).
    ........
      r4936 | fred | 2008-06-28 21:12:01 +0100 (Sat, 28 Jun 2008) | 3 lines
      
      Added a version of Adam's convert-font to handle variable width/height BDF fonts.  Generates two additional arrays suffixed .WTAB and .HTAB giving the
      widths and heights of each character-code.
    ........
      r4937 | ats | 2008-07-05 00:06:59 +0100 (Sat, 05 Jul 2008) | 5 lines
      
      Always use old-style implicit rules in the Makefile.ams, since BSD make doesn't
      like the new-style rules. This is trivial for everything except the CIF
      examples, which had a pattern rule with two dependencies; I've expanded those
      out.
    ........
      r4938 | ats | 2008-07-07 12:34:05 +0100 (Mon, 07 Jul 2008) | 2 lines
      
      Add missing rule for cif-commstime.
    ........
      r4939 | fred | 2008-07-13 01:09:24 +0100 (Sun, 13 Jul 2008) | 2 lines
      
      build script update to check writability of home-dir, small dtrace copyright updates.
    ........

--- runtime/ccsp/include/dtrace.h
+++ runtime/ccsp/include/dtrace.h
@@ -1,6 +1,6 @@
 /*
  *	dtrace.h -- debugging traces for CCSP
- *	Copyright (C) 2006 Fred Barnes <frmb@kent.ac.uk>
+ *	Copyright (C) 2006-2008 Fred Barnes <frmb@kent.ac.uk>
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
