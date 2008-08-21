7593e36bbecb4f4742c980af11bd61b6fee52883
Matt Jadud <matt@jadud.com>
2008-08-21 21:30:37 +0000

    A no-op firmware for the moment.

---
+++ tvm/fluke/firmware/firmware.h
@@ -0,0 +1,19 @@
+
+/* slinker binary2c output */
+static const int ws_size = 4;
+static const int vs_size = 0;
+static const int ms_size = 0;
+static const int inst_size = 12;
+static const unsigned char transputercode[] TVM_WORD_ALIGN = {
+ 0x20,
+ 0x20,
+ 0x20,
+ 0x20,
+ 0x20,
+ 0x20,
+ 0x20,
+ 0x00,  0x22,
+ 0xf0,  0x00,
+ 0x00 
+};
+
