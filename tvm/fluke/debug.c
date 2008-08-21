ff00c55de8863f073a73324542752a8e8f99e2c2
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-08-21 22:42:39 +0000

    lpc210x.h   - copied from fluke firmware.
    tvm.c
      - copied from srv1 directory
      - commented enough things out and made a few changes to get things to compile
    
    timer.c
    debug.c
    fluke.h
      - new files with very little interesting content
    
    main.c
      - call run_tvm... no actual HW setup yet
    
    Makefile
      - Things compile (on OS X at least) with compiler from:
        http://www.gnuarm.org/ (GCC-4.2 toolchain)

---
+++ tvm/fluke/debug.c
@@ -0,0 +1,14 @@
+
+
+
+void debug_print_chr (const unsigned char c)
+{
+}
+
+void debug_print_hex (unsigned int val)
+{
+}
+
+void debug_print_str (const char *str)
+{
+}
---
+++ tvm/fluke/timer.c
@@ -0,0 +1,14 @@
+#include "fluke.h"
+
+WORD arm7tdmi_get_time(ECTX ectx)
+{
+  return 0;
+}
+
+void sleep_until(WORD timeout)
+{
+}
+
+void sleep(void)
+{
+}
