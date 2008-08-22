61c634baa3a95650deb600bef26c3bc818946892
jadudm <jadudm@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-08-22 02:35:22 +0000

    All of the changes are minor, and were just us getting things to the point that we could say "this code does (or does not) run."
    As it turns out, it "does not." We spent most of the evening not working, so this last commit is just so Christian can take a look tomorrow while I go to a meeting. Very, very exciting.
    
    Things should just be builable with
    
    ARMGCC=<PATH> make download
    
    which will build everything (including libtvm for ARM) and load it onto the Fluke using lpc21sp (or whatever it is called).

--- tvm/fluke/main.c
+++ tvm/fluke/main.c
@@ -4,8 +4,35 @@
  * A port of the Transterpreter virtual machine to the IPRE Fluke.
  *
  **************************************/
+#include "setup.h"
 
 int main (void)
 {
-	return run_tvm();
+	int i,j;
+
+	setup_pins();
+	setup_pll();
+	setup_mam();
+	
+	led_on();
+	// `run_tvm();
+
+	j = 0;	
+	for(;;) 
+	{
+		led_off();
+		for(i=0;i<j;i++) 
+		{
+			__asm__ __volatile__ ("nop");
+		}
+		
+		led_on();
+		for(i=0;i<j;i++)
+		{
+			__asm__ __volatile__ ("nop");
+		}
+		j = j + 10000;
+		
+
+	}
 }
