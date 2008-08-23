9491a4111620e3988cfe357df6807d71b041cdbd
Matt Jadud <matt@jadud.com>
2008-08-23 13:03:23 +0000

    This is an interim commit. I discovered an example regarding interrupt-driven code on the LPC series by R O Software. Parts of code from this source are already included in the Fluke distribution, but with constants renamed or tweaked. I've imported the example wholesale into the directory "lpc".
    What this does is gives me a completely self-consistent set of headers in one
    place, that completely specify connection to the board. I've moved everything to
    use the new headers (I think), and will now continue to trim and comment out
    things in the "original" Fluke code that had used this in a piecemeal fashion.
    The end-result should be a more "textbook" port to the LPC2106, or, at least, a
    port that is in keeping with widely available, freely available example code
    that is used by many and pointed to from a number of fora.

--- tvm/fluke/fluke_uart.c
+++ tvm/fluke/fluke_uart.c
@@ -23,7 +23,8 @@
  * reduced to see what has to be done for minimum UART-support by mthomas
  *****************************************************************************/
 
-#include "lpc210x.h"
+#include "lpc/lpc210x.h"
+#include "fluke_uart.h"
 #include "fluke.h"
 
 /* on LPC210x: UART0 TX-Pin=P0.2, RX-Pin=P0.1 
@@ -41,59 +42,59 @@ void uart0Init(uint16_t baud, uint8_t mode, uint8_t fmode)
   // setup Pin Function Select Register (Pin Connect Block) 
   // make sure old values of Bits 0-4 are masked out and
   // set them according to UART0-Pin-Selection
-  PINSEL0 = (PINSEL0 & ~UART0_PINMASK) | UART0_PINSEL;
+  PINSEL0 = (PINSEL0 & ~U0_PINMASK) | U0_PINSEL;
 
-  UART0_IER = 0x00;             // disable all interrupts
-  UART0_IIR = 0x00;             // clear interrupt ID register
-  UART0_LSR = 0x00;             // clear line status register
+  U0IER = 0x00;             // disable all interrupts
+  U0IIR = 0x00;             // clear interrupt ID register
+  U0LSR = 0x00;             // clear line status register
 
   // set the baudrate - DLAB must be set to access DLL/DLM
-  UART0_LCR = (1<<UART0_LCR_DLAB); // set divisor latches (DLAB)
+  U0LCR = (1<<U0LCR_DLAB); // set divisor latches (DLAB)
 
-  UART0_DLL = (uint8_t)baud;         // set for baud low byte
-  UART0_DLM = (uint8_t)(baud >> 8);  // set for baud high byte
+  U0DLL = (uint8_t)baud;         // set for baud low byte
+  U0DLM = (uint8_t)(baud >> 8);  // set for baud high byte
   
   // set the number of characters and other
   // user specified operating parameters
   // Databits, Parity, Stopbits - Settings in Line Control Register
-  UART0_LCR = (mode & ~(1<<UART0_LCR_DLAB)); // clear DLAB "on-the-fly"
+  U0LCR = (mode & ~(1<<U0LCR_DLAB)); // clear DLAB "on-the-fly"
   // setup FIFO Control Register (fifo-enabled + xx trig) 
-  UART0_FCR = fmode;
+  U0FCR = fmode;
 }
 
 int uart0Putch(int ch)
 {
-  while (!(UART0_LSR & ULSR_THRE))          // wait for TX buffer to empty
+  while (!(U0LSR & ULSR_THRE))          // wait for TX buffer to empty
     continue;                           // also either WDOG() or swap()
 
-  UART0_THR = (uint8_t)ch;  // put char to Transmit Holding Register
+  U0THR = (uint8_t)ch;  // put char to Transmit Holding Register
   return (uint8_t)ch;      // return char ("stdio-compatible"?)
 }
 
 int uart0TxEmpty(void)
 {
-  return (UART0_LSR & (ULSR_THRE | ULSR_TEMT)) == (ULSR_THRE | ULSR_TEMT);
+  return (U0LSR & (ULSR_THRE | ULSR_TEMT)) == (ULSR_THRE | ULSR_TEMT);
 }
 
 void uart0TxFlush(void)
 {
-  UART0_FCR |= UFCR_TX_FIFO_RESET;          // clear the TX fifo
+  U0FCR |= UFCR_TX_FIFO_RESET;          // clear the TX fifo
 }
 
 
 /* Returns: character on success, -1 if no character is available */
 int uart0Getch(void)
 {
-  if (UART0_LSR & ULSR_RDR)                 // check if character is available
-    return UART0_RBR;                       // return character
+  if (U0LSR & ULSR_RDR)                 // check if character is available
+    return U0RBR;                       // return character
 
   return -1;
 }
 
 int uart0GetchBlock(void)
 {
-  while (!(UART0_LSR & ULSR_RDR));
-  return UART0_RBR;
+  while (!(U0LSR & ULSR_RDR));
+  return U0RBR;
 }
 
 
@@ -107,66 +108,66 @@ void uart1Init(uint16_t baud, uint8_t mode, uint8_t fmode)
   // setup Pin Function Select Register (Pin Connect Block) 
   // make sure old values of Bits 0-4 are masked out and
   // set them according to UART1-Pin-Selection
-  PINSEL0 = (PINSEL0 & ~UART1_PINMASK) | UART1_PINSEL;
+  PINSEL0 = (PINSEL0 & ~U1_PINMASK) | U1_PINSEL;
   
   // turn on CTS
-  PINSEL0 = (PINSEL0 & ~UART1_CTS_PINMASK) | UART1_CTS_PINSEL;
+  PINSEL0 = (PINSEL0 & ~U1CTS_PINMASK) | U1CTS_PINSEL;
   
-  UART1_IER = 0x00;             // disable all interrupts
-  UART1_IIR = 0x00;             // clear interrupt ID register
-  UART1_LSR = 0x00;             // clear line status register
+  U1IER = 0x00;             // disable all interrupts
+  U1IIR = 0x00;             // clear interrupt ID register
+  U1LSR = 0x00;             // clear line status register
 
   // set the baudrate - DLAB must be set to access DLL/DLM
-  UART1_LCR = (1<<UART1_LCR_DLAB); // set divisor latches (DLAB)
-  UART1_DLL = (uint8_t)baud;         // set for baud low byte
-  UART1_DLM = (uint8_t)(baud >> 8);  // set for baud high byte
+  U1LCR = (1<<U1LCR_DLAB); // set divisor latches (DLAB)
+  U1DLL = (uint8_t)baud;         // set for baud low byte
+  U1DLM = (uint8_t)(baud >> 8);  // set for baud high byte
   
   // set the number of characters and other
   // user specified operating parameters
   // Databits, Parity, Stopbits - Settings in Line Control Register
-  UART1_LCR = (mode & ~(1<<UART1_LCR_DLAB)); // clear DLAB "on-the-fly"
+  U1LCR = (mode & ~(1<<U1LCR_DLAB)); // clear DLAB "on-the-fly"
   // setup FIFO Control Register (fifo-enabled + xx trig) 
-  UART1_FCR = fmode;
+  U1FCR = fmode;
 }
 
 int uart1Putch(int ch)
 {
-  while (!(UART1_LSR & ULSR_THRE))    // wait for TX buffer to empty
+  while (!(U1LSR & ULSR_THRE))    // wait for TX buffer to empty
     continue;                         // also either WDOG() or swap()
 
-  UART1_THR = (uint8_t)ch;  // put char to Transmit Holding Register
+  U1THR = (uint8_t)ch;  // put char to Transmit Holding Register
   return (uint8_t)ch;       // return char ("stdio-compatible"?)
 }
 
 int uart1PutchCTS(int ch)
 {  
 
-  while (!(UART1_MSR & UMSR_CTS) || !(UART1_LSR & ULSR_THRE))  // wait for CTS to change
+  while (!(U1MSR & UMSR_CTS) || !(U1LSR & ULSR_THRE))  // wait for CTS to change
     {
       ;
     }
   
   
-  UART1_THR = (uint8_t)ch;  // put char to Transmit Holding Register
+  U1THR = (uint8_t)ch;  // put char to Transmit Holding Register
   
   return (uint8_t)ch;       // return char ("stdio-compatible"?)
 }
 
 int uart1TxEmpty(void)
 {
-  return (UART1_LSR & (ULSR_THRE | ULSR_TEMT)) == (ULSR_THRE | ULSR_TEMT);
+  return (U1LSR & (ULSR_THRE | ULSR_TEMT)) == (ULSR_THRE | ULSR_TEMT);
 }
 
 void uart1TxFlush(void)
 {
-  UART1_FCR |= UFCR_TX_FIFO_RESET;          // clear the TX fifo
+  U1FCR |= UFCR_TX_FIFO_RESET;          // clear the TX fifo
 }
 
 /* Returns: character on success, -1 if no character is available */
 int uart1Getch(void)
 {
-  if (UART1_LSR & ULSR_RDR)                 // check if character is available
-    return UART1_RBR;                       // return character
+  if (U1LSR & ULSR_RDR)                 // check if character is available
+    return U1RBR;                       // return character
 
   return -1;
 }
@@ -176,11 +177,11 @@ int uart1GetchRTS(void)
   uart1_set_rts();
 
   // check if character is available
-  if (UART1_LSR & ULSR_RDR)
+  if (U1LSR & ULSR_RDR)
     {
       uart1_clear_rts();
       // return character
-      return UART1_RBR;   
+      return U1RBR;   
     }
   
   
@@ -190,33 +191,33 @@ int uart1GetchRTS(void)
 
 int uart1GetchBlock(void)
 {
-  while (!(UART1_LSR & ULSR_RDR));
-  return UART1_RBR;
+  while (!(U1LSR & ULSR_RDR));
+  return U1RBR;
 }
 
 int uart1GetchBlockRTS(void)
 {
   uart1_set_rts();
-  while (!(UART1_LSR & ULSR_RDR));
+  while (!(U1LSR & ULSR_RDR));
 
   uart1_clear_rts();
   
-  return UART1_RBR;
+  return U1RBR;
 }
 
 inline void uart1_set_rts()
 {
-  //UART1_MCR |= UMCR_RTS;
+  //U1MCR |= UMCR_RTS;
   IOCLR = B_RTS;
 }
 
 inline void uart1_clear_rts()
 {
-  //UART1_MCR &= ~UMCR_RTS;
+  //U1MCR &= ~UMCR_RTS;
   IOSET = B_RTS;
 }
 
 inline int uart1_cts()
 {
-  return (UART1_MSR & UMSR_CTS);
+  return (U1MSR & UMSR_CTS);
 }
---
+++ tvm/fluke/lpc/config.h
@@ -0,0 +1,76 @@
+/******************************************************************************
+ *
+ * $RCSfile: $
+ * $Revision: $
+ *
+ * This module provides information about the project configuration
+ * Copyright 2004, R O SoftWare
+ * No guarantees, warrantees, or promises, implied or otherwise.
+ * May be used for hobby or commercial purposes provided copyright
+ * notice remains intact.
+ *
+ *****************************************************************************/
+#ifndef INC_CONFIG_H
+#define INC_CONFIG_H
+
+#include "types.h"
+#include "LPC210x.h"
+
+// some handy DEFINES
+#ifndef FALSE
+#define FALSE               0
+#ifndef TRUE
+#define TRUE                !FALSE
+#endif
+#endif
+
+#ifndef BIT
+#define BIT(n)              (1 << (n))
+#endif
+
+// declare functions and values from crt0.S & the linker control file
+extern void reset(void);
+// extern void exit(void);
+extern void abort(void);
+// maybe add interrupt vector addresses
+
+#define HOST_BAUD           (115200)
+
+#define WDOG()
+
+#define PCLK                (CCLK / PBSD) // Pheripheal Bus Clock Freq.
+#define PLL_MUL             (4)         // PLL Multiplier
+#define CCLK                (FOSC * PLL_MUL) // CPU Clock Freq.
+
+// Pheripheral Bus Speed Divider
+#define PBSD                2           // MUST BE 1, 2, or 4
+
+
+#if 0 /* Board specific parameters */
+// PLL setup values are computed within the LPC include file
+// It relies upon the following defines
+#define FOSC                (14745000)  // Master Oscillator Freq.
+
+// Do some value range testing
+#if ((FOSC < 10000000) || (FOSC > 25000000))
+#error Fosc out of range (10MHz-25MHz)
+#error correct and recompile
+#endif
+
+#if ((CCLK < 10000000) || (CCLK > 60000000))
+#error cclk out of range (10MHz-60MHz)
+#error correct PLL_MUL and recompile
+#endif
+
+#if ((FCCO < 150000000) || (FCCO > 320000000))
+#error Fcco out of range (156MHz-320MHz)
+#error internal algorithm error
+#endif
+
+#if ((PBSD != 1) && (PBSD != 2) && (PBSD != 4))
+#error Pheripheal Bus Speed Divider (PBSD) illegal value (1, 2, or 4)
+#endif
+
+#endif /* Board specific parameters */
+
+#endif /* config.h */
