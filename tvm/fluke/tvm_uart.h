4360ea0754e8bc07ccfaed58bd9a7b8296982c6b
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-08-24 02:26:35 +0000

    We have a new and improved fluke directory

--- tvm/fluke/tvm_uart.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef TVM_UART_H
-#define TVM_UART_H
-
-int uart0_in (ECTX ectx, WORD count, BYTEPTR pointer);
-int uart0_out (ECTX ectx, WORD count, BYTEPTR pointer);
-
-#endif /* TVM_UART_H */
