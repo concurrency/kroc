b83bf6e4d942daae13566398343a175fd5b26074
jadudm <jadudm@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2008-08-26 13:46:49 +0000

    I reorganized a few things in fluke.c. Still compiles, might still run.

--- tvm/fluke/fluke.c
+++ tvm/fluke/fluke.c
@@ -7,28 +7,53 @@
  */
 #include "fluke.h"
 
-static tvm_t 		tvm;
-static tvm_ectx_t 	firmware_ctx, user_ctx;
+/*{{{ Machine state */
+static tvm_t        tvm;
+static tvm_ectx_t   firmware_ctx, user_ctx;
+/*}}}*/
+
+/*{{{  User context state */
+static BYTEPTR    user_bytecode;
+static WORD       user_bytecode_len;
+static WORDPTR    user_memory;
+static WORD       user_memory_len;
+static WORDPTR    user_parent         = (WORDPTR) NOT_PROCESS_P;
+/*}}}*/
+
+
+/*{{{ Removed Code
+ * init_uart:
+ *  This was removed because this is provided by the RO Software
+ *  code. There is nothing TVM specific about UART setup.
+ *
+ * handle_int10:
+ * handle_int14:
+ *  These are the ISRs for the UART in the SRV1 firmware.
+ *  We can look at them there for refernce. The timer interrupt
+ *  exploration already encapsulates a fair bit of this.
+ *
+ * complete_uart0_tx_interrupt, complete_uart0_rx_interrupt:
+ *  We may want to look at these again. However, they aren't needed
+ *  here at this time.
+ * 
+ * run_output:
+ *  I removed the interrupt-aware version. We will have to integrate
+ *  interrupts into our UART code, but I'd like to lift the abstraction
+ *  layer while doing it, if possible.
+ * 
+ * srv_modify_sync_flags:
+ *  We may need something just like it. But, we'll bring it back in
+ *  and see if we can make it a bit more cross-platform.
+ }}} */
+
+
+/*{{{ Interrupts */
 
 static void arm7tdmi_modify_sync_flags (ECTX ectx, WORD set, WORD clear)
 {
 	/* Fine, until we enable interrupts... */
 	ectx->sflags = (ectx->sflags & (~clear)) | set;
 }
-
-#if 0
-/*{{{  Scheduling support */
-static void srv_modify_sync_flags (ECTX ectx, WORD set, WORD clear)
-{
-	unsigned short imask;
-	
-	DISABLE_INTERRUPTS (imask);
-
-	ectx->sflags = (ectx->sflags & (~clear)) | set;
-
-	ENABLE_INTERRUPTS (imask);
-}
-#endif
 
 static void clear_pending_interrupts (void)
 {
@@ -52,229 +77,9 @@ static void clear_pending_interrupts (void)
 }
 /*}}}*/ 
 
-
-/*{{{  TVM UART definitions */
-
-#define CTS_PIN		6
-#define	RTS_PIN		0
-#define CTS_MASK	(1 << CTS_PIN)
-#define RTS_MASK	(1 << RTS_PIN)
-
-static volatile unsigned char    rx_buffer    = '\0';
-static WORDPTR                   rx_channel   = (WORDPTR) NOT_PROCESS_P;
-static volatile short            rx_pending   = 0;
-static volatile BYTEPTR          rx_ptr       = (BYTEPTR) NULL_P;
-
-static WORDPTR                   tx_channel   = (WORDPTR) NOT_PROCESS_P;
-static volatile WORD             tx_pending   = 0;
-static BYTEPTR                   tx_ptr       = (BYTEPTR) NULL_P;
-
-/*{{{ init_uart */
-/* 20080825 MCJ My assumption here is that we already
- * have good code that sets up the UARTs in the ROS layer.
+/*{{{ run_output
+ * This is a blocking (non-interrupt driven) UART0 putch.
  */
-#if 0
-void init_uart (void)
-{
-	unsigned char temp;
-
-	/* Configure port H for flow control CTS */
-	*pPORTHIO_DIR	|= CTS_MASK;
-	/* Configure port H for flow control RTS */
-	*pPORTHIO_INEN	|= RTS_MASK;
-	*pPORTHIO_POLAR	|= RTS_MASK;
-	/* Raise CTS to block input */
-	*pPORTHIO_SET	= CTS_MASK;
-	
-	/* Enable UART pins on port F */
-	*pPORTF_FER 	|= 0x000f;
-
-	/* Enable UART0 clocks */
-	*pUART0_GCTL	= UCEN;
-	/* Switch on divisor programming */
-	*pUART0_LCR	= DLAB;
-	/* Program divisor */
-	*pUART0_DLL	= UART0_DIVIDER;
-	*pUART0_DLH	= UART0_DIVIDER >> 8;
-	/* Set operational mode (disables divisor programming) */
-	*pUART0_LCR	= WLS(8); /* 8 bit, no parity, one stop bit */
-
-	/* Reads to clear possible pending errors / irqs */
-	temp = *pUART0_RBR;
-	temp = *pUART0_LSR;
-	temp = *pUART0_IIR;
-	SSYNC;
-
-	/* Enable receive interrupts on IVG 10 */
-	*pSIC_IAR1	= (*pSIC_IAR1 & ~(0xf << 0xc)) | P11_IVG(10);
-	*pSIC_IMASK	|= IRQ_DMA8;
-	*pUART0_IER	|= ERBFI;
-	SSYNC;
-
-	/* Setup RTS interrupts on IVG 14, but don't enable */
-	*pSIC_IAR2	= (*pSIC_IAR2 & ~(0xf << 0x4)) | P17_IVG(14);
-	*pSIC_IMASK	|= IRQ_PFA_PORTH;
-	SSYNC;
-}
-#endif
-/*}}}*/
-
-/*{{{ handle_uart_interrupt: We only get one UART interrupt on the ARM.
- *    So, we handle and jump to the appropriate routine
- *    (ie. RX or TX)
- */
-#if 0
-#define INTERRUPT_HANDLER_PREAMBLE()     ISR_ENTRY();
-#define INTERRUPT_ID_REG                 U0IIR
-#define NO_INTERRUPTS                    UIIR_NO_INT
-#define INTERRUPT_ID_MASK                UIIR_ID_MASK
-#define RECEIVE_LINE_STATUS_INT          UIIR_RLS_INT
-#define U0_LINE_STATUS_REG               U0LSR
-void handle_uart_interrupt (void) 
-{
-	uint8_t interrupt_id;
-	interrupt_id = U0_INT_ID_REG;
-
-	INTERRUPT_HANDLER_PREAMBLE();
-
-	// Loop until there are no interrupts left to handle.
-	while ((interrupt_id & NO_INTERRUPTS) == 0)
-	{
-		switch (interrupt_id & INTERRUPT_ID_MASK)
-		{
-			case RECEIVE_LINE_STATUS_INT:
-				// Read the U0 line status register to clear it.
-				U0_LINE_STATUS_REG;
-				break;
-			/* TODO: Receive interrupts */
-			
-
-}
-#endif
-/*}}}*/
-
-/*{{{ handle_int10 - This is the incoming (RX) interrupt. */
-#if 0
-void handle_int10 (void)
-{
-	unsigned char buffer;
-
-	/* Raise (clear) CTS */
-	*pPORTHIO_SET	= CTS_MASK;
-
-	/* Read character (clears interrupt condition) */
-	buffer		= *pUART0_RBR;
-
-	/* Is anything waiting for the character? */
-	if (rx_ptr != (BYTEPTR) NULL_P) {
-		/* Yes; give it the data and trigger requeue */
-		write_byte (rx_ptr, (BYTE) buffer);
-		rx_ptr		= (BYTEPTR) NULL_P;
-		raise_tvm_interrupt (TVM_INTR_UART0_RX);
-	} else {
-		/* No; buffer the character */
-		rx_buffer	= buffer;
-		rx_pending++;
-	}
-}
-#endif 
-/*}}}*/
-// handle_int10: 
-//   Clear CTS (*pPORTHIO_SET = CTS_MASK)
-//   Read character (buffer = *pUART0_RBR)
-
-/*{{{ handle_int14 - This is the outbound (TX) interrupt.
- *    RENAMED: handle_transmit_interrupt */
-#if 0
-
-// On the ARM, is this equivalent to clearing the FIFO?
-#define CLEAR_UART0_RTS     0;
-#define INVALIDATE_MEMORY   0;
-
-
-void handle_transmit_interrupt (void)
-{
-
-	if (!(tx_pending = run_output (tx_pending, &tx_ptr))) {
-		/* Complete; notify TVM and disable interrupt */
-		raise_tvm_interrupt (TVM_INTR_UART0_TX);
-
-		// *pPORTHIO_MASKA_CLEAR = RTS_MASK;
-		CLEAR_UART0_RTS;
-		
-		// Invalidate memory
-		// SSYNC;
-		INVALIDATE_MEMORY;
-	}
-}
-#endif
-/*}}}*/
-// handle_int14:
-//   Clear RTS (*pPORTHIO_MASKA_CLEAR = RTS_MASK)
-
-/*{{{ complete_uart0_rx_interrupt - This completes the RX interrupt. */
-#if 0
-void complete_uart0_rx_interrupt (ECTX ectx)
-{
-	ectx->add_to_queue (ectx, rx_channel);
-	rx_channel = NOT_PROCESS_P;
-}
-#endif
-/*}}}*/
-
-/*{{{ complete_uart0_tx_interrupt - This completes the TX interrupt. */
-#if 0
-void complete_uart0_tx_interrupt (ECTX ectx)
-{
-	ectx->add_to_queue (ectx, tx_channel);
-	tx_channel = NOT_PROCESS_P;
-}
-#endif
-/*}}}*/
-
-/*{{{ uart0_is_blocking - Non-zero if rx or tx is NOT_PROCESS_P */
-#if 0
-int uart0_is_blocking (void)
-{
-	/* Return non-zero if rx_channel or tx_channel is not NOT_PROCESS_P. */
-	return ((int) rx_channel | (int) tx_channel) ^ NOT_PROCESS_P;
-}
-#endif
-/*}}}*/
-
-/*{{{ run_output - Does the send. */
-#if 0
-static WORD run_output (WORD count, BYTEPTR *pptr)
-{
-	BYTEPTR ptr = *pptr;
-
-	while (count && (*pPORTHIO & RTS_MASK)) {
-		unsigned char c = read_byte (ptr);
-
-		ptr = byteptr_plus (ptr, 1);
-
-		while (!(*pUART0_LSR & THRE)) {
-			continue;
-		}
-
-		/* Send data */
-		*pUART0_THR = c;
-
-		count--;
-	}
-
-	*pptr = ptr;
-
-	return count;
-}
-#endif
-/*}}}*/
-// run_output:
-//   Check ready: (*pPORTHIO & RTS_MASK)
-//   Buffer ready: (*pUART0_LSR & THRE)
-//   Send data: (*pUART0_THR = c)
-
-/* Non-interrupt driven "run_output" */
 static WORD run_output (WORD count, BYTEPTR *pptr)
 {
 	BYTEPTR ptr = *pptr;
@@ -288,63 +93,20 @@ static WORD run_output (WORD count, BYTEPTR *pptr)
 
 	return count;
 }
+/*}}}*/
 
+/*{{{ uart0_in 
+ * Does nothing.
+ */
 int uart0_in (ECTX ectx, WORD count, BYTEPTR pointer)
 {
-#if 0
-	unsigned short imask;
-	int reschedule;
-	
-	DISABLE_INTERRUPTS (imask);
-
-	if (rx_pending) {
-		write_byte (pointer, (BYTE) rx_buffer);
-		rx_pending	= 0;
-		BARRIER;
-		reschedule	= 0;
-	} else {
-		rx_channel	= ectx->wptr;
-		rx_ptr		= pointer;
-		BARRIER;
-		reschedule	= 1;
-	}
-
-	ENABLE_INTERRUPTS (imask);
-
-	if (reschedule) {
-		/* Lower (set) CTS */
-		*pPORTHIO_CLEAR = CTS_MASK;
-		/* Save instruction pointer */
-		WORKSPACE_SET (ectx->wptr, WS_IPTR, (WORD) ectx->iptr);
-		/* Reschedule */
-#endif 
 		return ectx->run_next_on_queue (ectx);
-#if 0
-	} else {
-		return ECTX_CONTINUE;
-	}
-#endif
 }
+/*}}}*/
 
-/* Original UART putch routine. */
-#if 0
-void uart0_send_char (const unsigned char c)
-{
-	/* Wait for RTS to go low (remote ready) */
-	while (!(*pPORTHIO & RTS_MASK)) {
-		continue;
-	}
-
-	/* Wait for UART0 send buffer to be ready */
-	while (!(*pUART0_LSR & THRE)) {
-		continue;
-	}
-
-	/* Send data */
-	*pUART0_THR = c;
-}
-#endif 
-
+/*{{{ uart0_out
+ * Blocking.
+ */
 int uart0_out (ECTX ectx, WORD count, BYTEPTR pointer)
 {
 	if (count == sizeof(WORD)) 
@@ -362,38 +124,11 @@ int uart0_out (ECTX ectx, WORD count, BYTEPTR pointer)
 		 * for an interrupt-driven UART. We're going
 		 * to (for the moment) have a blocking UART
 		 */
-#if 0
-		if (count > 0) 
-		{
-			/* Couldn't ship all the output, hand the
-			 * rest to the interrupt handler.
-			 */
-			tx_channel	= ectx->wptr;
-			tx_ptr		= pointer;
-			tx_pending	= count;
-			
-			BARRIER;
-
-			/* Enable interrupt */
-			*pPORTHIO_MASKA_SET = RTS_MASK;
-
-			/* Reschedule */
-			WORKSPACE_SET (ectx->wptr, WS_IPTR, (WORD) ectx->iptr);
-			return ectx->run_next_on_queue (ectx);
-		} else {
-			/* Finished. Note this does not handle the case
-			 * where "count" is negative. It shouldn't be, but
-			 * we have no guarantees.
-			 */
-		}
-#endif
 	}
-
+/*}}}*/
 	return ECTX_CONTINUE;
 }
 
-
-/* }}} */
 
 /*{{{  External channel definitions */
 static EXT_CHAN_ENTRY	ext_chans[] = {
@@ -411,36 +146,12 @@ static EXT_CHAN_ENTRY	ext_chans[] = {
 		.mt_in      = NULL,
 		.mt_out     = NULL
 	},
-
-#if 0
-	{
-		.typehash	= 0,
-		.in		= ppi_dma_in,
-		.out		= ppi_dma_out,
-		.mt_in		= ppi_dma_mt_in,
-		.mt_out		= NULL
-	},
-	{
-		.typehash	= 0,
-		.in		= twi_in,
-		.out		= twi_out,
-		.mt_in		= twi_mt_in,
-		.mt_out		= twi_mt_out
-	}
-#endif
 };
 
 static const int	ext_chans_length =
 				sizeof(ext_chans) / sizeof(EXT_CHAN_ENTRY);
 /*}}}*/
 
-/*{{{  User context state */
-static BYTEPTR		user_bytecode;
-static WORD		user_bytecode_len;
-static WORDPTR		user_memory;
-static WORD		user_memory_len;
-static WORDPTR 		user_parent	= (WORDPTR) NOT_PROCESS_P;
-/*}}}*/
 
 /*{{{  Firmware functions for running user bytecode */
 /* PROC firmware.run.user (VAL []BYTE bytecode, VAL INT ws, vs, ms, 
