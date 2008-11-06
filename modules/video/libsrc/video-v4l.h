24f4ed996e85cea8ff5f080a7d784e9077a2afc5
C.G. Ritson <cgr@kent.ac.uk>
2008-11-06 12:38:07 +0000

    Working on making the 'video' module more generic:   * Generically have inputs, instead of just cameras.   * Do format flag conversion.   * Use req/resp instead of in/out.   * Fix the includes so that it builds on my system (needed 'ioctl.h').
    I haven't tested this yet, as I probably need to implement the V4L2 bits first.

--- modules/video/libsrc/video-v4l.h
+++ modules/video/libsrc/video-v4l.h
@@ -20,6 +20,21 @@
 #ifndef __VIDEO_V4L_H
 #define __VIDEO_V4L_H
 
+#define OPI_VIDEO_TYPE_CAMERA	0x1
+#define OPI_VIDEO_TYPE_TUNER	0x2
+#define OPI_VIDEO_FLAG_AUDIO	0x1
+
+#define OPI_VIDEO_PAL_INVALID   0
+#define OPI_VIDEO_PAL_GRAY      1
+#define OPI_VIDEO_PAL_HI240     2
+#define OPI_VIDEO_PAL_RGB565    3
+#define OPI_VIDEO_PAL_RGB24     4
+#define OPI_VIDEO_PAL_RGB32     5
+#define OPI_VIDEO_PAL_RGB555    6
+#define OPI_VIDEO_PAL_YUV422    7
+#define OPI_VIDEO_PAL_YUYV      8
+#define OPI_VIDEO_PAL_YUV420P   15
+
 #define OPI_VIDEO_DEVICE_FNAMEMAX 128
 
 typedef struct opi_video_device {
@@ -36,17 +51,19 @@ typedef struct opi_video_identity {
 	int namelen;
 } opi_video_identity_t;
 
-#define OPI_VIDEO_CAMINPUT_NAMEMAX 64
+#define OPI_VIDEO_INPUT_NAMEMAX 64
 
-typedef struct opi_video_caminput {
+typedef struct opi_video_input {
 	int id;
-	char name[OPI_VIDEO_CAMINPUT_NAMEMAX];
+	char name[OPI_VIDEO_INPUT_NAMEMAX];
 	int namelen;
+	int type;
+	int flags;
 	int minw;
 	int minh;
 	int maxw;
 	int maxh;
-} opi_video_caminput_t;
+} opi_video_input_t;
 
 typedef struct opi_video_iodata {
 	int use_mmap;
