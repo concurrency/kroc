f3f53ba6f9c694f53016cab40c9867bf47f7fce5
Neil Brown <nccb@kent.ac.uk>
2009-03-23 15:33:54 +0000

    Added some documentation to Carl's resize function, pointing out that it doesn't fix the dimensions.

--- modules/cif/libsrc/cif.h
+++ modules/cif/libsrc/cif.h
@@ -162,6 +162,17 @@ static inline mt_cb_t *MTAllocChanType (Workspace wptr, int channels, bool share
 }
 /*}}}*/
 /*{{{ mt_array_t *MTResize1D (Workspace wptr, mt_array_t *array, int new_size) */
+
+/**
+ * Resizes a one-dimensional mobile array, either in-place or by allocating a
+ * new array and copying across all the data that will fit inside the new
+ * array.  You should use the return pointer instead of the one you passed in
+ * (since the array might have been re-allocated in a new location).  The
+ * final parameter is the new length (in terms of array elements).  This
+ * function does *not* (necessarily) modify the dimensions of the mt_array_t,
+ * so you should do that yourself after the function has returned.
+ *
+ */
 static inline mt_array_t *MTResize1D (Workspace wptr, mt_array_t *array, int new_size)
 {
 	return (mt_array_t *) MTResize (wptr, MT_RESIZE_DATA, array, new_size);
