d1b6b53ad1038fbefe68f037626adf76d2c44409
Adam Sampson <ats@offog.org>
2009-07-09 17:51:47 +0000

    When doing memory layout, check that we haven't run out of memory, and terminate with an appropriate message if we have.

--- tvm/arduino/sketch/tbc.c
+++ tvm/arduino/sketch/tbc.c
@@ -102,7 +102,7 @@ static int avr_tbc_decode (const prog_char *data, UWORD length, ECTX context, WO
 	WORDPTR ws, vs;
 
 	avr_tenc_element_t element;
-	int ret;
+	int ret, memory_used;
 
 	/* Decode the required elements */
 	if ((ret = load_uint (&data, &length, "ws U", &ws_size)) < 0)
@@ -133,13 +133,22 @@ static int avr_tbc_decode (const prog_char *data, UWORD length, ECTX context, WO
 	}
 #endif
 
-	tvm_ectx_layout (
+	memory_used = tvm_ectx_layout (
 		context, memory,
 		"", 0,
 		ws_size, vs_size,
 		&ws, &vs
 	);
-	/* FIXME: check memory hasn't been exhausted */
+
+#ifdef DEBUG
+	printf ("loaded program with ws_size=%d, vs_size=%d, memory_size=%d, used=%d\n", ws_size, vs_size, memory_size, memory_used);
+#endif
+
+	/* Check we haven't exhausted memory. */
+	if (memory_used > memory_size) {
+		terminate ("not enough RAM for program", NULL);
+	}
+
 	ret = tvm_ectx_install_tlp (
 		context, tvm_addr_from_progmem ((prog_void *) bytecode),
 		ws, vs,
