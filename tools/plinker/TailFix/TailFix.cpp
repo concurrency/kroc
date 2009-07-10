48dfa58da66b99af90dac4e4bf59463dc3ece941
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2009-07-10 18:08:54 +0000

    This is an LLVM pass which fixes up the bytecode so that tail call generation works correctly after optimisation. Without this pass most optimisation passes destroy the semantics required for the backend generator to produce proper tail calls. Technically this is the backend generators fault (to at least a large extent). The optimiser allows passes to be loaded (from dynamic libraries) at runtime so this pass can just be slotted in to an existing install of LLVM. I now just need to figure out where in the tree to put it, and how to make it build outside the LLVM sources.

---
+++ tools/plinker/TailFix/TailFix.cpp
@@ -0,0 +1,101 @@
+/*
+ *  LLVM Pass for Fixing Tail Calls (Prior to Native Code Generation)
+ *  Copyright (C) 2009  Carl Ritson <cgr@kent.ac.uk>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Instructions.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/ADT/SmallPtrSet.h"
+
+using namespace llvm;
+
+namespace {
+	struct TailFix : public FunctionPass {
+
+		static char ID;
+		TailFix() : FunctionPass(&ID) {}
+
+		virtual bool runOnFunction(Function &F) {
+			SmallVector<BasicBlock*, 16> fixUpBlocks;
+			bool modified = false;
+
+			llvm::cerr << "TailFix: " << F.getName() << "\n";
+
+			for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+				/* Do we have a tail call? */
+				CallInst* callInst;
+				if (!(callInst = dyn_cast<CallInst>(&*I)))
+					continue;
+				if (!callInst->isTailCall())
+					continue;
+				
+				++I;
+
+				/* Followed by a branch? Or unreachable? */
+				if (BranchInst* firstBranchInst = dyn_cast<BranchInst>(&*I)) {
+					/* Follow the branch (and further branches),
+					 * looking for a 'ret void'.
+					 */
+					BranchInst* branchInst = firstBranchInst;
+					bool fixUp = false;
+					do {
+						BasicBlock *successor 	= branchInst->getSuccessor (0);
+						Instruction &first 	= successor->front();
+						
+						branchInst = NULL;
+
+						if (ReturnInst *returnInst = dyn_cast<ReturnInst>(&first)) {
+							if (returnInst->getNumOperands() == 0)
+								fixUp = true;
+						} else if ((branchInst = dyn_cast<BranchInst>(&first))) {
+							if (!(branchInst->isUnconditional()))
+								branchInst = NULL;
+						}
+					} while (branchInst);
+
+					/* If we found ret void then fix up is required */
+					if (fixUp)
+						fixUpBlocks.push_back(firstBranchInst->getParent());
+
+				} else if (UnreachableInst* unreachableInst = dyn_cast<UnreachableInst>(&*I)) {
+					fixUpBlocks.push_back(unreachableInst->getParent());
+				}
+
+				--I;
+			}
+		
+			/* Remove branches and replace with 'ret void' */
+			for (SmallVector<BasicBlock*, 16>::iterator I = fixUpBlocks.begin(), E = fixUpBlocks.end(); I != E; ++I) {
+				BasicBlock *BB = *I;
+
+				BB->getInstList().pop_back();
+				ReturnInst::Create(NULL, BB);
+				
+				modified = true;
+			}
+
+			return modified;
+		}
+	};
+}
+
+char TailFix::ID = 0;
+static RegisterPass<TailFix> X("tailfix", "Tail Call Fixup Pass");
+
