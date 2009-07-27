992e7b478a6aaacbf5b02b8cc0fd40772dd9e062
C.G. Ritson <cgr@kent.ac.uk>
2009-07-27 08:31:46 +0000

    Fix typo in comment.

--- distribution/osx/build_kroc.sh
+++ distribution/osx/build_kroc.sh
@@ -28,7 +28,7 @@ if [ "$?" == "1" ] ; then
   curl -O http://www.libsdl.org/release/$SDL.tar.gz || exit 1
   tar -xvzf $SDL.tar.gz
   cd $SDL
-  # Compiling with newer zquartz (xquartz.macosforge.org)
+  # Compiling with newer xquartz (xquartz.macosforge.org)
   # does not work w/o this patch
   patch -p0 < ../../patch-SDL_x11gl_c.h.diff
   ./configure --prefix=$INSTALL
