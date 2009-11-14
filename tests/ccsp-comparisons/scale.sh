c7adbb4c94226730881eb39da39deea8bcb07d69
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2009-11-14 22:42:14 +0000

    CPU layout isn't as I expected.

--- tests/ccsp-comparisons/scale.sh
+++ tests/ccsp-comparisons/scale.sh
@@ -5,8 +5,11 @@ TESTS="$1 $2 $3 $4 $5 $6"
 for ((i = 1; i <= 8; i = i + 1)); do
   export CORES=$i
   cpus="0"
-  for ((j = 2; j < (2*$CORES); j = j + 2)); do
+  for ((j = 2; j < (2*$CORES) && j < 8; j = j + 2)); do
   	cpus="$cpus,$j"
+  done
+  for ((j = 1; j < (2*($CORES-4)) && j < 8; j = j + 2)); do
+	cpus="$cpus,$j"
   done
   echo taskset --cpu-list "$cpus" ./bench.sh ${CORES}c $TESTS
   taskset --cpu-list "$cpus" ./bench.sh ${CORES}c $TESTS
