40345c79f93dd7566837ded9fd073561c18061dc
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-01-29 16:36:26 +0000

    build the xslt library

--- distribution/windows/build.py
+++ distribution/windows/build.py
@@ -13,7 +13,18 @@ libxml2 = Package('libxml2',
         configArgs=['--without-threads', 
                     '--prefix=' + os.path.join(ROOT, 'install')])
 
-b = Builder(libxml2)
+libxslt = Package('libxslt',
+        url='ftp://xmlsoft.org/libxslt/libxslt-1.1.26.tar.gz',
+	srcPath=os.path.join(ROOT, 'libxslt-1.1.26'),
+		buildPath=os.path.join(ROOT, 'libxslt-1.1.26'),
+	buildCfg=Autotools,
+        configArgs=['--with-libxml-src=' + os.path.join(ROOT, 'libxml2-2.7.6'),
+                    '--prefix=' + os.path.join(ROOT, 'install')],
+        deps = [libxml2]
+)
+
+b = Builder(libxslt)
 b.download()
 b.unarchive()
 b.build()
+#b.install()
