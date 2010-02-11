ae0e1814762c4a9da319697b1384b060b5abb877
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-02-11 15:49:54 +0000

    Tweaking scripts.

--- tvm/nxt/get_nxos.sh
+++ tvm/nxt/get_nxos.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ ! -e nxos ]; then
 	git clone http://github.com/perlfu/nxos.git nxos
 fi
 
 if [ -e nxos ]; then
-	pushd nxos/nxos
-	scons
-	popd
-fi
-
-if [ -e nxos/nxos/libnxos.a ]; then
-	cp nxos/nxos/libnxos.a libnxos.a
+	scons -C nxos/nxos
 fi
 
