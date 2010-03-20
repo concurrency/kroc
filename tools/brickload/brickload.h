f6c6f2ccd74032d8ceb475ae628a28af13a86125
C.G. Ritson <cgr@kent.ac.uk>
2010-03-20 12:54:49 +0000

    Add empty shell for 'brickload' firmware and bytecode loading tool for RXT/NXT.

---
+++ tools/brickload/brickload.h
@@ -0,0 +1,7 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "config.h"
+
+
---
+++ tools/brickload/mac.c
@@ -0,0 +1,22 @@
+/* 
+ *  mac.c -- part of brickload NXT/RXT firmware and bytecode tool
+ *  Copyright (C) 2010  Carl Ritson <cgr@kent.ac.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "brickload.h"
+
+#ifdef OS_MAC
+#endif /* OS_MAC */
