7fd6e1b1acaf88dd73a0bc1f559e1198c6a87efc
jadudm <jadudm@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-06-16 15:28:12 +0000

    Looking for comments.
    1. Slowed TVM output down to 4800. Adjust 'read-arduino' accordingly.
    
    2. Added "softserial.module," which wraps a setup PROC as well as a proc to transmit using the softserial_write FFI.
    
    3. Added an FFI call (slammed in for the moment in an ugly place -- ffi.c) that, in theory, does a serial transmit on a pin of the user's choosing (pins 2 - 7)
    
    I also grossly bumped the location of usercode to 0x5200 for testing purposes. Specifically, the firmware grew a bit.
    
    Using testtx.occ, I transmit values over the software serial link. I connect pin 3 on the transmitter to the RX pin on another Arduino. The receiver is running testrx.occ, which is using the interrupt-based serial routines that were in ledmatrix.occ. 
    
    Running at 57600, I can use the interrupt-based routines (both tx and rx) to talk to each-other. This is good. I can do the same with those slowed down. Also good. I cannot do this, though, when I use my software bitbang code.
    
    Any thoughts? Basically, I get pretty bogus values at the receiving end. They do increase linearly, but they're... wrong. Basically, the bottom half of the nibble seems to be correct, and the upper half of the nibble seems to be wrong.
    
    A bit stuck, really. We're about to ramp up on Mega support (which will give us more serial ports), but in the meantime I need a way for the students to send commands to a GPS while reading from it on the RX pin, and sending debug data back to the PC on the TX pin. Other brainstorms as to how solve that problem are welcome as well...

--- tvm/arduino/ffi.c
+++ tvm/arduino/ffi.c
@@ -1,4 +1,52 @@
 #include "tvm-arduino.h"
+#include "util/delay.h"
+
+#define BITDELAY9600      84  
+#define HALFBITDELAY9600  42
+#define BITDELAY4800     188
+#define HALFBITDELAY4800  94
+
+/* DDRD, PORTD, PINB are Arduino digital 0 - 7 
+ * Only use 2 - 7, as 0, 1 are already serial.
+ */
+
+int ffi_softserial_write (ECTX ectx, WORD args[])
+{
+	int  tx_pin = args[0];
+	int  byte   = args[1];
+	char mask;
+ 	
+	printf_P (PSTR ("S %d, %04x\n"), tx_pin, byte);
+	// vprintf(PSTR("p: %04x, b %04x\n"), tx_pin, byte);
+
+	cli();
+
+	/* Set pin as output */
+	DDRD |= (1 << tx_pin);
+
+	/* Pull the write pin low to start transmission */
+  PORTD &= ~(1 << tx_pin);
+
+	_delay_us(BITDELAY4800);
+
+  /* Bang out each bit on the pin */
+	for (mask = 0x01 ; mask ; mask <<= 1) {
+		if (byte & mask) {
+			PORTD |= (1 << tx_pin);
+		} else {
+			PORTD &= ~(1 << tx_pin);
+		}
+		_delay_us(BITDELAY4800);
+	}
+
+	/* Pull high to send stop bit.*/
+	PORTD |= (1 << tx_pin);
+	_delay_us(BITDELAY4800);
+	
+	sei();
+	
+	return SFFI_OK;
+}
 
 /* Copy a block of memory from the Transterpreter's possibly-virtual memory
    space into real memory. */
@@ -14,6 +62,8 @@ static void memcpy_from_tvm (BYTEPTR from, void *to, int size) {
 }
 
 SFFI_FUNCTION sffi_table[] = {
-	ffi_wait_for_interrupt
+	ffi_wait_for_interrupt,
+	ffi_softserial_write
 };
+
 const int sffi_table_length = sizeof(sffi_table) / sizeof(SFFI_FUNCTION);
