229cb12c5516840c7fb6d6565e7f1c5c21cf64c7
Christian Jacbosen <cljacobsen@gmail.com>
2010-06-29 11:45:06 +0000

    Trying to break out host specific stuff sensibly.
    For now it is only the detection of serial ports which is done, though the idea
    is that other things which are platform specific will also have a hook in here.
    This is all to try and make it easier to support multiple hosts without the
    nasty if(osx) elif (linux) elif ... stuff that got littered in the code before.

---
+++ tools/occplug/org/transterpreter/occPlug/hosts/BaseHost.java
@@ -0,0 +1,59 @@
+package org.transterpreter.occPlug.hosts;
+
+/*
+ * BaseHost.java
+ * part of the occPlug plugin for the jEdit text editor
+ * Copyright (C) 2010 Christian L. Jacobsen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+public class BaseHost {
+
+	/**
+	 * Provide a list of suggestions for available serial ports. 
+	 * This is just a list of suggestions and the user will be able to
+	 * enter their own port.
+	 * 
+	 * This implementation returns null, should be overridden to return sensible
+	 * suggestions if this is possible.
+	 * 
+	 * @return a list of serial port names or null if no suggestions.
+	 */
+	public String[] getSerialPorts()
+	{
+		return null;
+	}
+	
+	public static BaseHost getHostObject()
+	{
+		String os = System.getProperty("os.name");
+		
+		if(os.equals("OS X"))
+		{
+			return new OSX();
+		}
+		else if(os.equals("Windows"))
+		{
+			return new Windows();
+		}
+		else if(os.equals("Linux"))
+		{
+			return new Unix();
+		}
+		
+		throw new RuntimeException("Unknown operating system");
+	}
+}
