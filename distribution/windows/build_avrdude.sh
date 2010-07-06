dcd3d75d002c40b23a681d3bf9b154d6579da1e6
David Gilmore <gilmorenator@gmail.com>
2010-07-06 13:27:42 +0000

    Updated two URLs: 1) Other.urls Line 3 2) build_avrdude.sh Line 10
    This update fixes a problem with deprecated urls in the Windows Distribution.

--- distribution/windows/build_avrdude.sh
+++ distribution/windows/build_avrdude.sh
@@ -7,7 +7,7 @@ if ! [ -d build/avrdude-$VERSION ] ; then
   mkdir -p build/avrdude-$VERSION
   cd build
   curl -L -O \
-    http://mirrors.aixtools.net/sv/avrdude/avrdude-$VERSION.tar.gz \
+    http://www.very-clever.com/download/nongnu/avrdude/avrdude-$VERSION.tar.gz \
     || exit 1
   tar -xvzf avrdude-$VERSION.tar.gz
   cd avrdude-$VERSION
