661330041b672e72db40e46ccac909705d334440
jadudm <jadudm@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-07-14 12:27:09 +0000

    These are continued changes working towards a package we can distribute. I hadn't fixed the installation variables correctly last night, but that was because I was in a huge hurry.

--- distribution/deb-pkg/config.py
+++ distribution/deb-pkg/config.py
@@ -42,8 +42,8 @@ BASE = {'YMD'            : now.strftime("%Y%m%d"),
 				'FINAL_DEST_ROOT'      : '@FINAL@',
 				'FINAL_DEST_BIN'       : '@FINAL@/bin',
 				'FINAL_DEST_SHARE'     : '@FINAL@/share',
-				'FINAL_DEST_FIRMWARE'  : '@DEST_SHARE@/firmwares',
-				'FINAL_DEST_CONF'      : '@DEST_SHARE@/conf',
+				'FINAL_DEST_FIRMWARE'  : '@FINAL_DEST_SHARE@/firmwares',
+				'FINAL_DEST_CONF'      : '@FINAL_DEST_SHARE@/conf',
 				'FINAL_DEST_LIB'       : '@FINAL@/lib',
 				# For building different targets
 				# Changing these default values requires you to
