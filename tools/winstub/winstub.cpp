db83c07e0c94a22299ece4135ef90d49161d47c1
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-07-15 16:11:33 +0000

    Use custom settings directory (APPDATA/Transterpreter/jEdit)
    Fix message dialogs that had titles and message inverted

--- tools/winstub/winstub.cpp
+++ tools/winstub/winstub.cpp
@@ -1,8 +1,31 @@
 #include <windows.h> 
 #include <tchar.h>
+#include <Shlobj.h>
+#include <string.h>
+
+#define MAX_CMDLINE MAX_PATH + 100
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
 {
+	TCHAR szAppDataPath[MAX_PATH];
+	TCHAR szCmdlineTmp[MAX_CMDLINE];
+	if(FAILED(SHGetFolderPath(NULL, 
+		CSIDL_APPDATA, 
+		NULL, 
+		0, 
+		szAppDataPath))) 
+	{
+		MessageBox(0,
+			"SHGetFolderPath returned FALSE",
+			"Could not start The Transterpreter",
+			MB_OK);
+		return 1;
+	}
+
+	strncat(szAppDataPath, "\\Transterpreter\\jEdit\"", MAX_PATH);
+	strncpy(szCmdlineTmp, " -jar jEdit\\jedit.jar -settings=\"", MAX_CMDLINE);
+	strncat(szCmdlineTmp, szAppDataPath, MAX_CMDLINE);
+
 	/*
 	 * At some point we should check the registry for which java is the current one,
 	 * check this documentation for the registry API:
@@ -10,7 +33,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	 * and bootstrap.py for how to find the current java using registry keys.
 	 */
 	LPTSTR szCmd = _tcsdup(TEXT("C:\\windows\\system32\\java.exe"));
-	LPTSTR szCmdline = _tcsdup(TEXT(" -jar jEdit\\jedit.jar"));
+	LPTSTR szCmdline = _tcsdup(szCmdlineTmp);
 
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
@@ -32,9 +55,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		&pi))
 	{
 		MessageBox(0,
-			"Could not start The Transterpreter",
 			"CreateProcess returned FALSE",
-			MB_OK); 
+			"Could not start The Transterpreter",
+			MB_OK);
+		return 1;
 	}
 
 	// I don't think there is a good reason for this process to be hanging around
