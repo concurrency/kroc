6b337e99b5496b9252904ffa9671d0fb377d8455
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-07-15 22:25:30 +0000

    Include and reference the correct runtime files

--- distribution/windows/build_python.py
+++ distribution/windows/build_python.py
@@ -8,20 +8,41 @@ class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
         # for the versioninfo resources
-        self.version = "0.6.1"
-        self.company_name = "No Company"
-        self.copyright = "no copyright"
-        self.name = "py2exe sample files"
+        self.version = "0.0.0.0"
+        self.company_name = ""
+        self.copyright = "Copyright 2010 The Transterpreter"
+        self.name = ""
 
-#data_files = [("Microsoft.VC90.CRT", glob(r'c:\dev\ms-vc-runtime\*.*'))]
-# C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT
-data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
+msvc90crt_manifest = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<!-- Copyright (c) Microsoft Corporation.  All rights reserved. -->
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <noInheritable/>
+    <assemblyIdentity
+        type="win32"
+        name="Microsoft.VC90.CRT"
+        version="9.0.21022.8"
+        processorArchitecture="x86"
+        publicKeyToken="1fc8b3b9a1e18e3b"
+    />
+    <file name="msvcr90.dll" /> <file name="msvcp90.dll" /> <file name="msvcm90.dll" />
+</assembly>
+'''
+f = open('build/Microsoft.VC90.CRT.manifest', 'w')
+f.write('msvc90crt_manifest')
+f.close()
+
+data_files = [
+    ("Microsoft.VC90.CRT", 
+     ['build/Microsoft.VC90.CRT.manifest'] + 
+     glob(r'C:\WINDOWS\WinSxS\x86_Microsoft.VC90.CRT*9.0.21022.8*\msvcr90.dll'))
+]
 
 manifest_template = '''
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
 <assemblyIdentity
-    version="5.0.0.0"
+    version="0.0.0.0"
     processorArchitecture="x86"
     name="%(prog)s"
     type="win32"
@@ -40,15 +61,15 @@ manifest_template = '''
     </dependentAssembly>
 </dependency>
 <dependency> 
-<dependentAssembly> 
-<assemblyIdentity 
-type="win32" 
-name="Microsoft.VC90.CRT" 
-version="9.0.30729.4148" 
-processorArchitecture="x86" 
-publicKeyToken="1fc8b3b9a1e18e3b" 
-/> 
-</dependentAssembly> 
+    <dependentAssembly> 
+        <assemblyIdentity 
+            type="win32" 
+            name="Microsoft.VC90.CRT" 
+            version="9.0.21022.8" 
+            processorArchitecture="x86" 
+            publicKeyToken="1fc8b3b9a1e18e3b" 
+        /> 
+    </dependentAssembly> 
 </dependency> 
 </assembly>
 '''
@@ -75,5 +96,5 @@ setup(
 	console=[
 		occbuild
 ],
-#data_files=data_files
+data_files=data_files
 )
