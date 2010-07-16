dcebd4fb88ab904dd1c2b26036e9a64f0a03d3f4
Christian Jacbosen <cljacobsen@gmail.com>
2010-07-16 15:06:31 +0000

    Got rid of a bunch of options which have long been unused.

--- tools/occplug/org/transterpreter/occPlug/options/OccPlugOptionPaneCmd.java
+++ tools/occplug/org/transterpreter/occPlug/options/OccPlugOptionPaneCmd.java
@@ -38,14 +38,9 @@ import org.gjt.sp.jedit.jEdit;
 import org.transterpreter.occPlug.OccPlugPlugin;
 
 public class OccPlugOptionPaneCmd extends AbstractOptionPane {
-	// private JCheckBox showPath;
-	private JTextField	skrocCmdName;
-	private JTextField	tvmCmdName;
-	private JTextField	firmdlCmdName;
-	private JTextField	dllCmdName;
-	private JTextField	libPath;
-	private JComboBox	legoTowerPort;
+
 	private JCheckBox	saveOnCompile;
+	private JCheckBox	verbose;
 
 	// private FontSelector font;
 
@@ -54,183 +49,30 @@ public class OccPlugOptionPaneCmd extends AbstractOptionPane {
 	}
 
 	public void _init() {
-		/*
-		 * showPath = new JCheckBox(jEdit.getProperty(
-		 * OccPlugPlugin.OPTION_PREFIX + "show-filepath.title"),
-		 * jEdit.getProperty(QuickNotepadPlugin.OPTION_PREFIX +
-		 * "show-filepath").equals("true")); addComponent(showPath);
-		 */
-
-		/*
-		 * pathName = new JTextField(jEdit.getProperty(
-		 * QuickNotepadPlugin.OPTION_PREFIX + "filepath")); JButton pickPath =
-		 * new JButton(jEdit.getProperty( QuickNotepadPlugin.OPTION_PREFIX +
-		 * "choose-file")); pickPath.addActionListener(this);
-		 * 
-		 * JPanel pathPanel = new JPanel(new BorderLayout(0, 0));
-		 * pathPanel.add(pathName, BorderLayout.CENTER); pathPanel.add(pickPath,
-		 * BorderLayout.EAST);
-		 * 
-		 * addComponent(jEdit.getProperty( QuickNotepadPlugin.OPTION_PREFIX +
-		 * "file"), pathPanel);
-		 */
-
-		/* SKROC COMMAND */
-
-		skrocCmdName = new JTextField(jEdit
-				.getProperty(OccPlugPlugin.OPTION_PREFIX + "skrocCmd"));
-		JButton skrocCmdPickPath = new JButton(
-		// JButton occCmdPickPath = new JButton(jEdit.getProperty(
-				// OccPlugPlugin.OPTION_PREFIX + "choose-file"));
-				"Choose");
-		skrocCmdPickPath.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				String[] paths = GUIUtilities.showVFSFileDialog(null, null,
-						JFileChooser.OPEN_DIALOG, false);
-				if (paths != null) {
-					skrocCmdName.setText(paths[0]);
-				}
-			}
-		});
-
-		JPanel skrocCmdPathPanel = new JPanel(new BorderLayout(0, 0));
-		skrocCmdPathPanel.add(skrocCmdName, BorderLayout.CENTER);
-		skrocCmdPathPanel.add(skrocCmdPickPath, BorderLayout.EAST);
-
-		addComponent("skroc command:", skrocCmdPathPanel);
-		// addComponent("skroc command:");
-		// addComponent(skrocCmdPathPanel);
-
-		/* TVM COMMAND */
-
-		tvmCmdName = new JTextField(jEdit
-				.getProperty(OccPlugPlugin.OPTION_PREFIX + "tvmCmd"));
-		JButton tvmCmdPickPath = new JButton(
-		// JButton occCmdPickPath = new JButton(jEdit.getProperty(
-				// OccPlugPlugin.OPTION_PREFIX + "choose-file"));
-				"Choose");
-		tvmCmdPickPath.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				String[] paths = GUIUtilities.showVFSFileDialog(null, null,
-						JFileChooser.OPEN_DIALOG, false);
-				if (paths != null) {
-					tvmCmdName.setText(paths[0]);
-				}
-			}
-		});
-
-		JPanel tvmCmdPathPanel = new JPanel(new BorderLayout(0, 0));
-		tvmCmdPathPanel.add(tvmCmdName, BorderLayout.CENTER);
-		tvmCmdPathPanel.add(tvmCmdPickPath, BorderLayout.EAST);
-
-		addComponent("tvm command:", tvmCmdPathPanel);
-
-		/* FIRMDL3 COMMAND */
-
-		firmdlCmdName = new JTextField(jEdit
-				.getProperty(OccPlugPlugin.OPTION_PREFIX + "firmdlCmd"));
-		JButton firmdlCmdPickPath = new JButton(
-		// JButton occCmdPickPath = new JButton(jEdit.getProperty(
-				// OccPlugPlugin.OPTION_PREFIX + "choose-file"));
-				"Choose");
-		firmdlCmdPickPath.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				String[] paths = GUIUtilities.showVFSFileDialog(null, null,
-						JFileChooser.OPEN_DIALOG, false);
-				if (paths != null) {
-					firmdlCmdName.setText(paths[0]);
-				}
-			}
-		});
-
-		JPanel firmdlCmdPathPanel = new JPanel(new BorderLayout(0, 0));
-		firmdlCmdPathPanel.add(firmdlCmdName, BorderLayout.CENTER);
-		firmdlCmdPathPanel.add(firmdlCmdPickPath, BorderLayout.EAST);
-
-		addComponent("firmdl command:", firmdlCmdPathPanel);
-
-		/* DLL COMMAND */
-
-		dllCmdName = new JTextField(jEdit
-				.getProperty(OccPlugPlugin.OPTION_PREFIX + "dllCmd"));
-		JButton dllCmdPickPath = new JButton(
-		// JButton occCmdPickPath = new JButton(jEdit.getProperty(
-				// OccPlugPlugin.OPTION_PREFIX + "choose-file"));
-				"Choose");
-		dllCmdPickPath.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				String[] paths = GUIUtilities.showVFSFileDialog(null, null,
-						JFileChooser.OPEN_DIALOG, false);
-				if (paths != null) {
-					dllCmdName.setText(paths[0]);
-				}
-			}
-		});
-
-		JPanel dllCmdPathPanel = new JPanel(new BorderLayout(0, 0));
-		dllCmdPathPanel.add(dllCmdName, BorderLayout.CENTER);
-		dllCmdPathPanel.add(dllCmdPickPath, BorderLayout.EAST);
-
-		addComponent("dll command:", dllCmdPathPanel);
-
-		/* LEGO TOWER PORT */
-
-		String legoTowerPortOptions[] = { "DEFAULT", "USB", "COM1", "COM2",
-				"COM3", "COM4", "COM5", "COM6" };
-		legoTowerPort = new JComboBox(legoTowerPortOptions);
-		String legoTowerPortSelected = jEdit
-				.getProperty(OccPlugPlugin.OPTION_PREFIX + "legoTowerPort");
-		/*
-		 * for(int i = 0; i < legoTowerPortOptions.length; i++) {
-		 * if(legoTowerPortSelected.equals(legoTowerPortOptions) legoTowerPort.
-		 * }
-		 */
-		legoTowerPort.setSelectedItem(legoTowerPortSelected);
-		org.gjt.sp.util.Log.log(org.gjt.sp.util.Log.DEBUG, this, "Got: "
-				+ legoTowerPortSelected);
-
-		addComponent("Lego tower port:", legoTowerPort);
-
-		/* Lib Path */
-		libPath = new JTextField(jEdit.getProperty(OccPlugPlugin.OPTION_PREFIX
-				+ "MasterLibraryPath"));
-		addComponent("Library path: ", libPath);
-
 		/* SAVE BEFORE COMPILING ? */
 
 		boolean saveOnCompileState = jEdit.getBooleanProperty(
 				OccPlugPlugin.OPTION_PREFIX + "saveOnCompile", true);
-
 		saveOnCompile = new JCheckBox(
 				"Automatically save source files when compiling");
 		saveOnCompile.setSelected(saveOnCompileState);
-
 		addComponent(saveOnCompile);
 
+		/* VERBOSE */
+
+		boolean verboseState = jEdit.getBooleanProperty(
+				OccPlugPlugin.OPTION_PREFIX + "verbose", true);
+		verbose = new JCheckBox(
+				"Enable verbose output");
+		verbose.setSelected(verboseState);
+		addComponent(verbose);
 	}
 
 	public void _save() {
-
-		jEdit.setProperty(OccPlugPlugin.OPTION_PREFIX + "skrocCmd",
-				skrocCmdName.getText());
-		jEdit.setProperty(OccPlugPlugin.OPTION_PREFIX + "tvmCmd", tvmCmdName
-				.getText());
-		jEdit.setProperty(OccPlugPlugin.OPTION_PREFIX + "firmdlCmd",
-				firmdlCmdName.getText());
-		jEdit.setProperty(OccPlugPlugin.OPTION_PREFIX + "dllCmd", dllCmdName
-				.getText());
-		jEdit.setProperty(OccPlugPlugin.OPTION_PREFIX + "MasterLibraryPath",
-				libPath.getText());
-		/*
-		 * jEdit.setBooleanProperty( "options.occPlug.slinker.optimalPrefixing",
-		 * slinkerOptimalPrefix.getModel().isSelected());
-		 */
-		jEdit.setProperty(OccPlugPlugin.OPTION_PREFIX + "legoTowerPort",
-				(String) legoTowerPort.getSelectedItem());
 		jEdit.setBooleanProperty(OccPlugPlugin.OPTION_PREFIX + "saveOnCompile",
 				saveOnCompile.isSelected());
-		org.gjt.sp.util.Log.log(org.gjt.sp.util.Log.DEBUG, this, "Set: "
-				+ legoTowerPort.getSelectedItem());
+		jEdit.setBooleanProperty(OccPlugPlugin.OPTION_PREFIX + "verbose",
+				verbose.isSelected());
 	}
 	// end AbstractOptionPane implementation
 
