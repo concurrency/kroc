ba5ff48650a11ae7a653ef83f3677d4b438d8f22
Christian Jacbosen <cljacobsen@gmail.com>
2010-07-17 04:50:15 +0000

    Automate the building of the updater + dependencies (WinSparkle)

---
+++ distribution/windows/build_updater.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ROOT=$PWD
+
+if ! [ -d build/winsparkle ] ; then
+  mkdir -p build/winsparkle
+  cd build
+   curl -O -L \
+     http://lyderjacobsen.org/misc/vslavik-winsparkle-v0.2-9-g941987c.zip \
+   || exit 1
+  cd winsparkle
+  unzip ../vslavik-winsparkle-v0.2-9-g941987c.zip
+  cd vslavik-winsparkle-941987c
+  cd 3rdparty
+  /c/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild.exe \
+    WinSparkleDeps.sln
+  cd ..
+  /c/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild.exe \
+    WinSparkle.sln
+fi
+
+cd $ROOT  
+cd ../../tools/winupdater
+
+ant -Djedit.install.dir=$ROOT/build/jedit/jedit-program/ \
+    -Dinstall.dir=$ROOT/build/macupdater \
+    -Dwinsparkle.dir=$ROOT/build/winsparkle/vslavik-winsparkle-941987c/
+
