08d13a11d6d1f5434d2df399b36ef146ca5fa901
Christian Jacbosen <cljacobsen@gmail.com>
2010-07-19 00:41:29 +0000

    Make it possible to pass kwargs to the command things

--- distribution/common/python-lib/distribution_tools/command.py
+++ distribution/common/python-lib/distribution_tools/command.py
@@ -1,13 +1,13 @@
 import subprocess
 import sys
 
-def execute(command):
+def execute(command, **kwargs):
     print 'running command: %s' % (' '.join(command), )
-    ret = subprocess.call(command)
+    ret = subprocess.call(command, **kwargs)
     if ret != 0:
         print 'command failed with error code %d' % (ret, )
         sys.exit(ret)
 
-def capture(command):
+def capture(command, **kwargs):
     print 'capturing command: %s' % (' '.join(command), )
-    return subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0]
+    return subprocess.Popen(command, stdout=subprocess.PIPE, **kwargs).communicate()[0]
