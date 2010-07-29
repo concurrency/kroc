55bcf76b99b13a8c1b3ee81f01666c2527cc1db7
jadudm <jadudm@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-07-29 01:01:21 +0000

    These changes are first steps towards full serial support on the ATmega1280 series.
    This is, in part, so I can do a full, clean build.

--- tvm/arduino/interrupts.c
+++ tvm/arduino/interrupts.c
@@ -2,6 +2,8 @@
 
 static int num_waiting = 0;
 
+/*{{{ ATmega328 enumeration*/
+#if defined(atmega328p)
 enum {
 	vintr_INT0 = 0,
 	vintr_INT1,
@@ -16,6 +18,44 @@ enum {
 	vintr_USART_TX,
 	NUM_INTERRUPTS
 };
+/*}}}*/
+/*{{{ ATmega1280 enumeration */
+#elif defined(atmega1280)
+enum {
+	vintr_INT0 = 0,
+	vintr_INT1,
+	vintr_INT2,
+	vintr_INT3,
+	vintr_INT4,
+	vintr_INT5,
+	vintr_INT6,
+	vintr_INT7,
+	vintr_PCINT0,
+	vintr_PCINT1,
+	vintr_PCINT2,
+	vintr_TIMER0,
+	vintr_TIMER1,
+	vintr_TIMER2,
+	vintr_TIMER3,
+	vintr_TIMER4,
+	vintr_TIMER5,	
+	vintr_ADC,
+	vintr_USART_RX0,
+	vintr_USART_RX1,
+	vintr_USART_RX2,
+	vintr_USART_RX3,
+	vintr_USART_UDRE0,
+	vintr_USART_UDRE1,
+	vintr_USART_UDRE2,
+	vintr_USART_UDRE3,
+	vintr_USART_TX0,
+	vintr_USART_TX1,
+	vintr_USART_TX2,
+	vintr_USART_TX3,
+	NUM_INTERRUPTS
+};
+#endif
+/*}}}*/
 
 typedef struct _vinterrupt {
 	/* Workspaces are always word-aligned, so we can use the least
@@ -24,6 +64,7 @@ typedef struct _vinterrupt {
 	WORDPTR wptr;
 	WORD pending;
 } vinterrupt;
+
 static vinterrupt interrupts[NUM_INTERRUPTS];
 
 void init_interrupts () {
@@ -110,6 +151,9 @@ MAP_SIMPLE_INTERRUPT(PCINT2_vect, vintr_PCINT2)
 MAP_SIMPLE_INTERRUPT(TIMER1_OVF_vect, vintr_TIMER1)
 MAP_SIMPLE_INTERRUPT(TIMER2_OVF_vect, vintr_TIMER2)
 MAP_SIMPLE_INTERRUPT(ADC_vect, vintr_ADC)
+
+/*{{{ ATmega328 serial interrupts */
+#if defined(atmega328p)
 ISR(USART_RX_vect) {
 	/* Disable the interrupt to stop it firing again immediately. */
 	UCSR0B &= ~_BV (RXCIE0);
@@ -123,6 +167,45 @@ ISR(USART_UDRE_vect) {
 	handle_interrupt (&interrupts[vintr_USART_UDRE]);
 }
 MAP_SIMPLE_INTERRUPT(USART_TX_vect, vintr_USART_TX)
+/*}}}*/
+/*{{{ ATmega1280 serial interrupts */
+#elif defined(atmega1280)
+#define SERIAL_INTERRUPT(vect_RX, vintr_RX, \
+												 vect_TX, vintr_TX, \
+												 vect_UDRE, vintr_UDRE, \
+												 UCSRnB, RXCIEn, UDRIEn) \
+	ISR(vect_RX) { \
+	UCSRnB &= ~_BV (RXCIEn); \
+	handle_interrupt (&interrupts[vintr_RX]); \
+	} \
+	ISR(vect_UDRE) { \
+	UCSRnB &= ~_BV (UDRIEn); \
+	handle_interrupt (&interrupts[vintr_UDRE]); \
+	} \
+	MAP_SIMPLE_INTERRUPT(vect_TX, vintr_TX)
+
+SERIAL_INTERRUPT(USART0_RX_vect, vintr_USART_RX0, 
+								 USART0_TX_vect, vintr_USART_TX0,
+								 USART0_UDRE_vect, vintr_USART_UDRE0,
+								 UCSR0B, RXCIE0, UDRIE0)
+
+SERIAL_INTERRUPT(USART1_RX_vect, vintr_USART_RX1, 
+								 USART1_TX_vect, vintr_USART_TX1,
+								 USART1_UDRE_vect, vintr_USART_UDRE1,
+								 UCSR1B, RXCIE1, UDRIE1)
+
+SERIAL_INTERRUPT(USART2_RX_vect, vintr_USART_RX2, 
+								 USART2_TX_vect, vintr_USART_TX2,
+								 USART2_UDRE_vect, vintr_USART_UDRE2,
+								 UCSR2B, RXCIE2, UDRIE2)
+
+SERIAL_INTERRUPT(USART3_RX_vect, vintr_USART_RX3, 
+								 USART3_TX_vect, vintr_USART_TX3,
+								 USART3_UDRE_vect, vintr_USART_UDRE3,
+								 UCSR3B, RXCIE3, UDRIE3)
+
+#endif
+/*}}}*/
 
 void clear_pending_interrupts () {
 	int i;
