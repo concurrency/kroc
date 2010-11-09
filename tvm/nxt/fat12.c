64609fc0544ee6131067cd27e7c4bfd4179efccc
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2010-11-09 15:18:56 +0000

    Functions to find files and extract them from FAT12 file system.

--- tvm/nxt/fat12.c
+++ tvm/nxt/fat12.c
@@ -96,7 +96,7 @@ struct _dir_ent_t {
 	uint16_t	mdate;
 	uint16_t	start;
 	uint32_t	length;
-};
+} __attribute__ ((packed));
 typedef struct _dir_ent_t dir_ent_t;
 
 /* FileStartSector = 
@@ -104,42 +104,120 @@ typedef struct _dir_ent_t dir_ent_t;
  * (MaxRootEntry(0x11) * 32 / BytesPerSector(0x0b)) + 
  * ((X − 2) * SectorsPerCluster(0x0d)) */ 
 
+static uint16_t read_fat_entry (uint8_t *fat, uint16_t entry)
+{
+	uint32_t offset = (entry / 2) * 3;
+	uint32_t value = fat[offset + 0] | (fat[offset + 1] << 8) | (fat[offset + 2] << 16);
+
+	if ((entry % 2) == 0) {
+		value &= 0xfff;
+	} else {
+		value >>= 12;
+	}
+
+	return value;
+}
+
 void fat12_init (uint8_t *data, uint32_t len)
 {
+	bootsec_t *bs = (bootsec_t *) data;
+	uint32_t max_clusters;
+
+	/* This is a fixed 40k FAT12 file system with 16 root dir entries */
 	memset (data, 0, len);
-	/* FIXME: this is a fixed 40k FAT12 file system with 16 root dir entries */
 	memcpy (data +    0,      data_0_to_26, sizeof (data_0_to_26)); 
 	memcpy (data +   38,    data_38_to_190, sizeof (data_38_to_190)); 
 	memcpy (data +  510,   data_510_to_514, sizeof (data_510_to_514)); 
 	memcpy (data + 1024, data_1024_to_1026, sizeof (data_1024_to_1026)); 
-	memcpy (data + 1536, data_1536_to_1561, sizeof (data_1536_to_1561)); 
+	memcpy (data + 1536, data_1536_to_1561, sizeof (data_1536_to_1561));
+
+	/* Rewrite the total sectors to match length */
+	bs->total_sectors 	= len / bs->bytes_per_sector;
+	max_clusters 		= ((bs->sectors_per_fat * bs->bytes_per_sector) * 2) / 3;
+
+	if ((bs->total_sectors / bs->sectors_per_cluster) > max_clusters)
+		bs->total_sectors = max_clusters * bs->sectors_per_cluster;
 }
 
-int fat12_decode (uint8_t *data, uint32_t len)
+int fat12_extract_file (uint8_t *data, uint32_t len, int fn, uint8_t *dst)
 {
-	bootsec_t 	*bs = (bootsec_t *) data;
+	bootsec_t 	*bs		= (bootsec_t *) data;
+	uint8_t		*data_end	= data + len;
+	
 	uint32_t 	ds;
-	uint8_t		*data_end = data + len;
+	uint32_t	bpc, bpf, to_read;
+	uint16_t	nc;
+	uint8_t		*fat, *da;
+	dir_ent_t	*root_dir, *de;
+	
+	bpc	= bs->bytes_per_sector * bs->sectors_per_cluster;
+	bpf	= bs->bytes_per_sector * bs->sectors_per_fat;
+	ds	= bs->reserved_sectors 
+			+ (bs->sectors_per_fat * bs->n_fats)
+			+ ((bs->root_dir_entries * 32) / bs->bytes_per_sector);
+	fat	= data + (bs->bytes_per_sector * bs->reserved_sectors);
+	root_dir= (dir_ent_t *) (fat + (bs->n_fats * bpf));
+	da	= ((uint8_t *) root_dir) + (bs->root_dir_entries * 32);
+	
+	if (fat >= data_end)
+		return -1;
+	if ((fat + bpf) >= data_end)
+		return -1;
+	if (((uint8_t *) root_dir) >= data_end)
+		return -1;
+	if (((uint8_t *) (root_dir + (bs->root_dir_entries * 32))) >= data_end)
+		return -1;
+	if (fn < 0 || fn >= bs->root_dir_entries)
+		return -1;
+
+	de	= &(root_dir[fn]);
+	to_read = de->length;
+	nc	= de->start;
+
+	while (to_read) {
+		uint32_t len = to_read > bpc ? bpc : to_read;
+		
+		if (((nc / 2) * 3) > bpf)
+			return -1;
+		
+		memcpy (dst, da + (nc * bpc), len);
+		
+		nc 	= read_fat_entry (fat, nc);
+		to_read -= len;
+	}
+	
+	return de->length;
+}
+
+int fat12_find_file (uint8_t *data, uint32_t len, char *ext, uint32_t *file_len)
+{
+	bootsec_t 	*bs		= (bootsec_t *) data;
+	uint8_t		*data_end	= data + len;
+	uint32_t	best_mtime	= 0;
+	int		best		= -1;
+
+	uint32_t 	ds;
 	uint8_t		*fat;
 	dir_ent_t	*root_dir;
+	int		i;
 
-	ds = 	bs->reserved_sectors 
-		+ (bs->sectors_per_fat * bs->n_fats)
-		+ ((bs->root_dir_entries * 32) / bs->bytes_per_sector);
-	fat =	data + (bs->bytes_per_sector * bs->reserved_sectors);
-	root_dir = (dir_ent_t *) (fat + (bs->bytes_per_sector * bs->n_fats * bs->sectors_per_fat));
+	ds	= bs->reserved_sectors 
+			+ (bs->sectors_per_fat * bs->n_fats)
+			+ ((bs->root_dir_entries * 32) / bs->bytes_per_sector);
+	fat	= data + (bs->bytes_per_sector * bs->reserved_sectors);
+	root_dir= (dir_ent_t *) (fat + (bs->bytes_per_sector * bs->n_fats * bs->sectors_per_fat));
 	
 	#ifdef FAT12_DEBUG
 	fprintf (stderr,
 		"oem_name = \"%c%c%c%c%c%c%c%c\"\n"
 		"bytes_per_sector = %d, sectors_per_cluster = %d\n"
-		"reserved_sectors = %d\n"
+		"reserved_sectors = %d, total_sectors = %d\n"
 		"n_fats = %d, sectors_per_fat = %d\n"
 		"root_dir_entries = %d\n",
 		bs->oem_name[0], bs->oem_name[1], bs->oem_name[2], bs->oem_name[3],
 		bs->oem_name[4], bs->oem_name[5], bs->oem_name[6], bs->oem_name[7],
 		bs->bytes_per_sector, bs->sectors_per_cluster,
-		bs->reserved_sectors,
+		bs->reserved_sectors, bs->total_sectors,
 		bs->n_fats, bs->sectors_per_fat,
 		bs->root_dir_entries
 	);
@@ -154,14 +232,53 @@ int fat12_decode (uint8_t *data, uint32_t len)
 	if (((uint8_t *) (root_dir + (bs->root_dir_entries * 32))) >= data_end)
 		return -1;
 
-	return 0;
+	for (i = 0; i < bs->root_dir_entries; ++i) {
+		dir_ent_t *de = &(root_dir[i]);
+		if (!(de->name[0] == 0x00 || de->name[0] == 0xe5)) {
+			#ifdef FAT12_DEBUG
+			fprintf (stderr, 
+				"root[%d] = \"%c%c%c%c%c%c%c%c\".\"%c%c%c\"\n"
+				"\tstart = %d, length = %d\n"
+				"\tmdate = %04x, mtime = %04x\n",
+				i,
+				de->name[0], de->name[1], de->name[2], de->name[3],
+				de->name[4], de->name[5], de->name[6], de->name[7],
+				de->ext[0], de->ext[1], de->ext[2],
+				de->start, de->length,
+				de->mdate, de->mtime
+			);
+			#endif
+			
+			if ((de->ext[0] == ext[0]) 
+					&& (de->ext[1] == ext[1]) 
+					&& (de->ext[2] == ext[2])) {
+				uint32_t mtime = (de->mdate << 16) | (de->mtime);
+				if (mtime > best_mtime) {
+					best 		= i;
+					best_mtime 	= mtime;
+					if (file_len)
+						*file_len = de->length;
+				}
+			}
+		} else {
+			#ifdef FAT12_DEBUG
+			fprintf (stderr, "root[%d] = empty (%02x)\n", 
+				i, de->name[0]
+			);
+			#endif
+		}
+	}
+	
+	return best;
 }
 
 #ifdef FAT12_DEBUG
 int main (int argc, char *argv[]) {
+	uint32_t f_len;
 	uint8_t *data;
 	size_t len;
 	FILE *fh = NULL;
+	int fn;
 
 	if (argc >= 2) {
 		char *file = argv[1];
@@ -185,15 +302,24 @@ int main (int argc, char *argv[]) {
 		}
 	} else {
 		fprintf (stdout, "Using built-in file system for testing.\n");
-		len = 40 * 1024;
+		len = 60 * 1024;
 		data = (uint8_t *) malloc (len);
 		fat12_init (data, len);
 	}
 
-	fprintf (stdout, "fat12_decode (%p, %ld) = %d\n",
-		data, len,
-		fat12_decode (data, (uint32_t) len)
+	fn = fat12_find_file (data, (uint32_t) len, "TBC", &f_len);
+	fprintf (stdout, "fat12_find_file (%p, %ld) = %d\n",
+		data, len, fn
 	);
+	if (fn >= 0) {
+		uint8_t *f_data = (uint8_t *) malloc (f_len);
+		int ret = fat12_extract_file (data, (uint32_t) len, fn, f_data);
+		fprintf (stdout, "fat12_decode_file (%p, %ld, %d, %p) = %d\n",
+			data, len, fn, f_data,
+			ret
+		);
+		free (f_data);
+	}
 
 	if (fh) {
 		munmap (data, len);
