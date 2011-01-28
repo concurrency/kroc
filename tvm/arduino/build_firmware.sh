207864e00dbf1b262bbc6190ab6f78829eab8716
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2011-01-28 17:46:44 +0000

    Arduino: A script to build firmware for the targets in occam/conf. Needs some changes in the configure script to accept a flag to set the filename of the produced firmware.

---
+++ tvm/arduino/build_firmware.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT=$0
+ROOT=`dirname $SCRIPT`
+
+CONFDIR=$ROOT/occam/conf
+CONFIGURE=$ROOT/configure
+
+targets=`ls $CONFDIR/*.conf.in`
+
+if [ -d build ] ; then
+	echo "Please remove the build directory to continue"
+	exit 1
+fi
+
+for target in $targets
+do
+	tname=`basename ${target%.*.*}`
+	tdir=build/$tname
+	mkdir -p $tdir
+	source $target
+	cd $tdir
+	../../$CONFIGURE --host=avr \
+			 --with-bytecode-addr=$TVM_BYTECODE_ADDR \
+			 --with-mcu=$TVM_GCC_MCU \
+			 --with-fcpu=$TVM_F_CPU \
+			 --with-fw-name=$TVM_ARDUINO_FIRMWARE \
+			 "$@"
+	make
+	make firmware.hex
+	cd ../../
+	mkdir -p output
+	cp build/target/$TVM_ARDUINO_FIRMWARE output/
+done
