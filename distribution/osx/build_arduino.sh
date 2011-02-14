8654f415216043563c44f805be3cd9d71519b20d
Christian Jacbosen <cljacobsen@gmail.com>
2011-02-14 16:50:15 +0000

    OS X Dist: Use the new Arduino 0022 tools (ie gcc) on mac

--- distribution/osx/build_arduino.sh
+++ distribution/osx/build_arduino.sh
@@ -9,8 +9,10 @@ if [ $windows ] ; then
 	ARDUINO=arduino-0018
 	ARDUINO_ZIP=$ARDUINO.zip
 else
-	ARDUINO=arduino-0016
-	ARDUINO_ZIP=$ARDUINO-mac.zip
+	ARDUINO=arduino-0022
+	ARDUINO_ZIP=$ARDUINO.dmg
+	GCC_PATH=Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin
+	IS_DMG=yes
 fi
 
 
@@ -20,11 +22,23 @@ BUILD=$PWD/build
 if ! [ -d $BUILD/$ARDUINO ] ; then
   mkdir -p build
   cd build
-  curl -O \
-    http://arduino.googlecode.com/files/$ARDUINO_ZIP \
-    || exit 1
-  unzip $ARDUINO_ZIP
-  #cd $ARDUINO
+  #curl -O \
+  #  http://arduino.googlecode.com/files/$ARDUINO_ZIP \
+  #  || exit 1
+  if [ $IS_DMG ]; then
+    mnt_point=`hdiutil attach -nobrowse -noautoopen $ARDUINO_ZIP | tail -n 1 \
+    | cut -f 3`
+    if [ "$?" -ne "0" ] ; then
+      echo "Could not attach disk image"
+      exit 1
+    fi
+    mkdir -p $ARDUINO
+    echo "Copying Arduino.app..."
+    cp -R $mnt_point/Arduino.app $ARDUINO
+    hdiutil detach $mnt_point
+  else
+    unzip $ARDUINO_ZIP
+  fi
 fi
 
 cd $BUILD
@@ -48,7 +62,7 @@ make
 make install
 
 OLD_PATH=$PATH
-PATH=$PATH:$BUILD/$ARDUINO/hardware/tools/avr/bin
+PATH=$PATH:$BUILD/$ARDUINO/$GCC_PATH
 cd $BUILD
 cd ../../../tvm/arduino
 unset ACLOCAL
