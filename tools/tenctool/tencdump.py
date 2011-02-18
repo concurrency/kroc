ae3988723c8348a1c517ab8787d3b9936f779834
clj <clj@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2011-02-18 10:28:47 +0000

    tenctool: Various changes
     * set executable bit
     * add #!
     * turn the individual tools into things that can be imported and used as modules

--- tools/tenctool/tencdump.py
+++ tools/tenctool/tencdump.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python
+
 import sys
 
 import tenclib
 
-if len(sys.argv) < 2:
-    print "Usage: %s <filename>" % sys.argv[0]
-    sys.exit(1)
+def dump(data):
+    parser = tenclib.TEncParser(data)
+    tenc   = parser.parse()
+    return str(tenc)
 
-data   = open(sys.argv[1], 'rb').read()
-parser = tenclib.TEncParser(data)
-tenc   = parser.parse()
-print tenc
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "Usage: %s <filename>" % sys.argv[0]
+        sys.exit(1)
+
+    data   = open(sys.argv[1], 'rb').read()
+    print dump(data)
--- tools/tenctool/tencid.py
+++ tools/tenctool/tencid.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python
+
 import sys
 
 import tenclib
 
-if len(sys.argv) < 3:
-    print "Usage: %s <tenc filename> <output filename or ->" % sys.argv[0]
-    sys.exit(1)
+def identity(data):
+    parser = tenclib.TEncParser(data)
+    tenc   = parser.parse()
+    return tenc.dump_bytes()
+ 
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "Usage: %s <tenc filename> <output filename or - (minus)>" % sys.argv[0]
+        sys.exit(1)
 
-data   = open(sys.argv[1], 'rb').read()
-parser = tenclib.TEncParser(data)
-tenc   = parser.parse()
-if sys.argv == '-':
-    sys.stdout.write(tenc.dump_bytes())
-else:
-    fp = open(sys.argv[2], 'wb')
-    fp.write(tenc.dump_bytes())
-    fp.close()
+    data   = open(sys.argv[1], 'rb').read()
+    data   = identity(data)
+    if sys.argv == '-':
+        sys.stdout.write(data)
+    else:
+        fp = open(sys.argv[2], 'wb')
+        fp.write(data)
+        fp.close()
