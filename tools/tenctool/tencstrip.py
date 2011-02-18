f1671432322f66722ea233cf93c2bc09ae1dc34d
Christian Jacbosen <cljacobsen@gmail.com>
2011-02-18 10:28:47 +0000

    tenctool: Various changes
     * set executable bit
     * add #!
     * turn the individual tools into things that can be imported and used as modules

--- tools/tenctool/tencstrip.py
+++ tools/tenctool/tencstrip.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 import sys
 
 import tenclib
 
 def usage():
     print "Usage: %s [-d] [-t] [-s] [-f] " \
-            "<tenc filename> <output filename or ->" % sys.argv[0]
+            "<tenc filename> <output filename or - (minus)>" % sys.argv[0]
     print "  default stripping is -d and -s"
     print "    -d: debug info"
     print "    -t: TLP info"
@@ -12,43 +14,50 @@ def usage():
     print "    -d: ffi table"
     sys.exit(1)
 
-if len(sys.argv) < 3 or len(sys.argv) > 7: 
-    usage()
-
-if len(sys.argv) == 3:
-    debug, tlp, symbol, ffi = False, True, False, True
-    args = sys.argv[1:]
-else:
-    debug, tlp, symbol, ffi = 4 * [True]
-    args = sys.argv[1:]
-    while args[0] in ['-d', '-t', '-s', '-f']:
-        if args[0]   == '-d': debug = False
-        elif args[0] == '-t': tlp = False
-        elif args[0] == '-s': symbol = False
-        elif args[0] == '-f': ffi = False
-        else: break
-        args = args[1:]
-if len(args) > 2:
-    usage()
-
-data   = open(args[0], 'rb').read()
-parser = tenclib.TEncParser(data)
-tenc   = parser.parse()
-
-def remove(tag):
+def remove(tenc, tag):
     for e in tenc.elements:
         if e.tag == 'tbcL':
             for e1 in e.elements:
                 if e1.tag == tag:
                     e1.elements = []
-if not debug: remove('dbgL')
-if not tlp: remove('tlpL')
-if not symbol: remove('stbL')
-if not ffi: remove('ffiL')
 
-if sys.argv == '-':
-    sys.stdout.write(tenc.dump_bytes())
-else:
-    fp = open(args[1], 'wb')
-    fp.write(tenc.dump_bytes())
-    fp.close()
+def strip(data, debug=True, symbol=True, tlp=False, ffi=False):
+    parser = tenclib.TEncParser(data)
+    tenc   = parser.parse()
+
+    if debug: remove(tenc, 'dbgL')
+    if tlp: remove(tenc, 'tlpL')
+    if symbol: remove(tenc, 'stbL')
+    if ffi: remove(tenc, 'ffiL')
+    return tenc.dump_bytes()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3 or len(sys.argv) > 7: 
+        usage()
+
+    if len(sys.argv) == 3:
+        debug, symbol, tlp, ffi = 2 * [True] + 2 * [False]
+        args = sys.argv[1:]
+    else:
+        debug, symbol, tlp, ffi = 4 * [False]
+        args = sys.argv[1:]
+        while args[0] in ['-d', '-t', '-s', '-f']:
+            if args[0]   == '-d': debug = True
+            elif args[0] == '-t': tlp = True
+            elif args[0] == '-s': symbol = True
+            elif args[0] == '-f': ffi = True
+            else: break
+            args = args[1:]
+    if len(args) > 2:
+        usage()
+
+    data   = open(args[0], 'rb').read()
+
+    data = strip(data, debug, symbol, tlp, ffi)
+
+    if sys.argv == '-':
+        sys.stdout.write(data)
+    else:
+        fp = open(args[1], 'wb')
+        fp.write(data)
+        fp.close()
