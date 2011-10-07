c416968a67a65ca17c5a571149c281b783be7384
cgr <cgr@ee8ce1bd-81f8-0310-a706-c2403d3965fe>
2011-10-07 14:16:18 +0000

    Switch to C++ interface for module linking as the boilerplate is simpler.

--- tvm/nacl/tvm_nacl.h
+++ tvm/nacl/tvm_nacl.h
@@ -1,37 +1,13 @@
 
 /*{{{  Includes */
-#ifdef HAVE_ERRNO_H
 #include <errno.h>
-#endif
-#ifdef HAVE_LIMITS_H
-#include <limits.h>
-#endif
-#ifdef HAVE_SIGNAL_H
 #include <signal.h>
-#endif
-#ifdef HAVE_STRING_H
 #include <string.h>
-#endif
-#ifdef HAVE_TIME_H
 #include <time.h>
-#endif
-#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
-
-#ifdef HAVE_SYS_IOCTL_H
-#include <sys/ioctl.h>
-#endif
-#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
-#endif
-#ifdef HAVE_SYS_TIMEB_H
 #include <sys/timeb.h>
-#endif
-
-#ifdef HAVE_TERMIOS_H
 #include <termios.h>
-#endif
 
 /*{{{  tvm_ectx_priv_t - ectx private data */
 #define TVM_ECTX_PRIVATE_DATA 	tvm_ectx_priv_t
